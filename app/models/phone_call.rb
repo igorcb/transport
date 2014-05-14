cc3a5c6beb2cad2d099d416553f8c71102c73e4c
igorcb <igor.batista@gmail.com>
2014-05-14 08:32:11 -0300

    modify length telefone

--- app/models/phone_call.rb
+++ app/models/phone_call.rb
@@ -2,7 +2,7 @@ class PhoneCall < ActiveRecord::Base
   belongs_to :type_service
 
   validates :nome, presence: true, length: { maximum: 100 } 
-  validates :telefone, presence: true, length: { maximum: 10 } 
+  validates :telefone, presence: true, length: { maximum: 50 } 
   validates :email, length: { maximum: 100 } 
   validates :type_service_id, presence: true, length: { maximum: 10 } 
   validates :assunto, presence: true
