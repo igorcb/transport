32543017ef62ecdd60912e1b580977cfc9a2bdb4
igorcb <igor.batista@gmail.com>
2014-05-30 09:30:10 -0300

    view pdf and image to browser

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -4,4 +4,20 @@ class Asset < ActiveRecord::Base
 	belongs_to :vehicle, class_name: "Vehicle", foreign_key: "asset_id", polymorphic: true, dependent: :destroy
 	belongs_to :employee, class_name: "Employee", foreign_key: "asset_id", polymorphic: true, dependent: :destroy
   has_attached_file :asset, styles: lambda { |a| a.instance.asset_content_type =~ %r(image) ? { medium: "300x300>", thumb: "120x90>", mini: "64x64>"} : {} }
+
+	def is_image?
+	  return false unless asset.content_type
+	  ['image/jpeg', 'image/pjpeg', 'image/gif', 'image/png', 'image/x-png', 'image/jpg'].include?(asset.content_type)
+	end  
 end
+
+
+# @@doc_types = {
+# :image => ['image/jpg','image/jpeg','image/pjpeg','image/png','image/x-png','image/gif'],
+# :pdf => ['application/pdf'],
+# :word => ['application/msword','applicationvnd.ms-word','applicaiton/vnd.openxmlformats-officedocument.wordprocessingm1.document'],
+# :excel => ['application/msexcel','application/vnd.ms-excel','application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'],
+# :powerpoint => ['application/mspowerpoint','application/vnd.ms-powerpoint','application/vnd.openxmlformats-officedocument.presentationml.presentation'],
+# :text => ['text/plain']
+
+# }
