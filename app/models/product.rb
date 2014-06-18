c3b1a3b44d68b786d0734e2a8aa9c496e114f50c
igorcb <igor.batista@gmail.com>
2014-06-18 11:46:15 -0300

    add validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,8 @@
 class Product < ActiveRecord::Base
+	validates :descricao, presence: true, length: { maximum: 100 } 
+	validates :category_id, presence: true
+	validates :cubagem, presence: true
+	
 	belongs_to :category
+
 end
