8a74285af6575f25e1e5b9829eebe03fa6dd8eec
igorcb <igor.batista@gmail.com>
2014-06-18 11:36:31 -0300

    add categories to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -70,6 +70,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:descricao, :cubagem)
+      params.require(:product).permit(:descricao, :category_id, :cubagem)
     end
 end
