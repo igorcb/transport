25b343f0fd269e488f5d9d3488241916c9e93a0f
igorcb <igor.batista@gmail.com>
2014-10-13 22:21:38 -0300

    add operation to menu application

--- app/controllers/operatings_controller.rb
+++ app/controllers/operatings_controller.rb
@@ -1,4 +1,5 @@
 class OperatingsController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_operating, only: [:show, :edit, :update, :destroy]
 
   # GET /operatings
