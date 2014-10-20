d9d9ab75b054f103ee4c04b6a217f72749961e6c
igorcb <igor.batista@gmail.com>
2014-10-20 16:28:07 -0300

    modifi account bank to refactoring

--- app/models/account_bank.rb
+++ app/models/account_bank.rb
@@ -1,6 +1,19 @@
 class AccountBank < ActiveRecord::Base
   
-
+  #belongs_to :bank, class_name: "Account", foreign_key: "account_id", polymorphic: true, dependent: :destroy
   belongs_to :driver, class_name: "Driver", foreign_key: "account_id", polymorphic: true, dependent: :destroy
+  belongs_to :client, class_name: "Client", foreign_key: "account_id", polymorphic: true, dependent: :destroy
 
+  before_save :select_bank
+  
+
+  def select_bank
+  	bank = Bank.find (self.banco)
+  	self.nome_banco = bank.nome
+  end
+
+  def cod_banco
+  	bank = Bank.find (self.banco)
+  	bank.banco
+  end
 end
