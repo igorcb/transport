80a78c4faeb7088f70418ea0d601d365f6d170aa
igorcb <igor.batista@gmail.com>
2014-11-17 09:31:31 -0300

    show data client and driver ordem_services

--- app/models/ordem_service_type_service.rb
+++ app/models/ordem_service_type_service.rb
@@ -1,6 +1,6 @@
 class OrdemServiceTypeService < ActiveRecord::Base
-  validates :ordem_service_id, presence: true
-  validates :type_service_id, presence: true
+  #validates :ordem_service, presence: true
+  validates :type_service, presence: true
   validates :valor, presence: true
 
   belongs_to :ordem_service
