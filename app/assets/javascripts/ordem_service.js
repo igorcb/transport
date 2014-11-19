688ee1ce961e3a1a6473b50c1cbb169257c10fe5
igorcb <igor.batista@gmail.com>
2014-11-19 16:37:59 -0300

    add plugin mask, add mask placa to ordemservices

--- app/assets/javascripts/ordem_service.js
+++ app/assets/javascripts/ordem_service.js
@@ -1,3 +1,10 @@
 $(document).ready(function(e) {
    $('#ordem_service_type_service, #ordem_service_nfe').nestedFields();
 });
+
+$(document).ready(function(e) {
+  $(".placa").mask("aaa-9999");
+//$(".data").mask("99/99/9999",{placeholder:"mm/dd/yyyy"});
+//$(".phone").mask("(999) 999-9999");  
+});
+
