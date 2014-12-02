f171f134656a562556b379340f89a969a4585853
igorcb <igor.batista@gmail.com>
2014-12-02 14:39:22 -0300

    modify presence qtde and date Ype to pallets

---
+++ db/migrate/20141202173444_modify_default_to_pallet.rb
@@ -0,0 +1,11 @@
+class ModifyDefaultToPallet < ActiveRecord::Migration
+  def up
+    execute "ALTER TABLE pallets ALTER COLUMN data_agendamento DROP NOT NULL"
+    execute "ALTER TABLE pallets ALTER COLUMN qtde DROP NOT NULL"
+  end
+  
+  def down
+    execute "ALTER TABLE pallets ALTER COLUMN data_agendamento SET NOT NULL"
+    execute "ALTER TABLE pallets ALTER COLUMN qtde SET NOT NULL"
+  end
+end
