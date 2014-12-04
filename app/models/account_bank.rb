ac95a06ddf2515d58136eedcb35bd1aeba955f27
igorcb <igor.batista@gmail.com>
2014-12-04 14:50:01 -0300

    add polymorphic association carrier, employee, supplier

--- app/models/account_bank.rb
+++ app/models/account_bank.rb
@@ -3,9 +3,11 @@ class AccountBank < ActiveRecord::Base
   #belongs_to :bank, class_name: "Account", foreign_key: "account_id", polymorphic: true, dependent: :destroy
   belongs_to :driver, class_name: "Driver", foreign_key: "account_id", polymorphic: true, dependent: :destroy
   belongs_to :client, class_name: "Client", foreign_key: "account_id", polymorphic: true, dependent: :destroy
+  belongs_to :carrier, class_name: "Carrier", foreign_key: "account_id", polymorphic: true, dependent: :destroy
+  belongs_to :employee, class_name: "Employee", foreign_key: "account_id", polymorphic: true, dependent: :destroy
+  belongs_to :supplier, class_name: "Supplier", foreign_key: "account_id", polymorphic: true, dependent: :destroy
 
   before_save :select_bank
-  
 
   def select_bank
   	bank = Bank.find (self.banco)
