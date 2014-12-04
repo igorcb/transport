fbfa8e75dacfdaa4db3f34862c1964eda49e3d74
igorcb <igor.batista@gmail.com>
2014-12-04 14:50:57 -0300

    add account_bank to carrier

--- app/models/carrier.rb
+++ app/models/carrier.rb
@@ -18,6 +18,10 @@ class Carrier < ActiveRecord::Base
   has_many :emails, class_name: "Email", foreign_key: "email_id", :as => :email, dependent: :destroy
   accepts_nested_attributes_for :emails, allow_destroy: true  
 
+  has_many :account_banks, class_name: "AccountBank", foreign_key: "account_id", :as => :contact, dependent: :destroy
+  accepts_nested_attributes_for :account_banks, allow_destroy: true
+  
   has_many :assets, as: :asset, dependent: :destroy
   accepts_nested_attributes_for :assets, allow_destroy: true, reject_if: :all_blank  
+
 end
