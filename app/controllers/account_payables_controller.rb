84463a32ea95174c89a8be7a3af6ff531b556d9b
igorcb <igor.batista@gmail.com>
2014-12-08 16:27:04 -0300

    add menu account_payables

--- app/controllers/account_payables_controller.rb
+++ app/controllers/account_payables_controller.rb
@@ -1,4 +1,5 @@
 class AccountPayablesController < ApplicationController
+  before_filter :authenticate_user!
   before_action :set_account_payable, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
