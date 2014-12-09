28556404fd566452e8f6000ed301f5ddb1055656
igorcb <igor.batista@gmail.com>
2014-12-09 16:14:52 -0300

    ordered id desc account_payable

--- app/controllers/account_payables_controller.rb
+++ app/controllers/account_payables_controller.rb
@@ -15,7 +15,7 @@ class AccountPayablesController < ApplicationController
   end
 
   def index
-    @account_payables = AccountPayable.all
+    @account_payables = AccountPayable.order('id desc')
     respond_with(@account_payables)
   end
 
