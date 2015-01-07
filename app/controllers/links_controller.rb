a1088f43bb39cf4f653baaaa346d95053ae2d2b0
igorcb <igor.batista@gmail.com>
2015-01-07 10:56:36 -0300

    add access control links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,7 @@
 class LinksController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /links
   # GET /links.json
