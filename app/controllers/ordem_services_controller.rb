df707f5043f43da88ac8bea5fb9b1c2fcb495f14
igorcb <igor.batista@gmail.com>
2015-01-13 09:56:39 -0300

    add agent to ordem_services

--- app/controllers/ordem_services_controller.rb
+++ app/controllers/ordem_services_controller.rb
@@ -147,7 +147,7 @@ class OrdemServicesController < ApplicationController
 
     def ordem_service_params
       params.require(:ordem_service).permit(:driver_id, :client_id, :data, :placa, :estado, :cidade, :cte, :danfe_cte, :valor_receita, :valor_despesas, :valor_liquido, 
-        :observacao, :status, :qtde_volume, :peso, :data_entrega_servico, :senha_sefaz,
+        :observacao, :status, :qtde_volume, :peso, :data_entrega_servico, :senha_sefaz, :carrier_id, 
         nfe_keys_attributes: [:nfe, :chave, :qtde, :id, :_destroy],
         ordem_service_type_service_attributes: [:ordem_service_id, :type_service_id, :valor, :qtde, :qtde_recebida, :id, :_destroy],
         assets_attributes: [:asset, :id, :_destroy]
