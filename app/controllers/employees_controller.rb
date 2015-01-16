6f8a9f8eabde96513b9d4f2eaa7aff1308b39e9b
igorcb <igor.batista@gmail.com>
2015-01-16 16:51:06 -0300

    add method get_employee_by_id to employee

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -75,6 +75,13 @@ class EmployeesController < ApplicationController
     end
   end
 
+  def get_employee_by_id
+    @employee = Employee.find(params[:id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_employee
