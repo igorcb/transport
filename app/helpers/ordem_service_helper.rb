da34ead3911c87b4898543100dfae12b24b961b2
igorcb <igor.batista@gmail.com>
2015-02-10 13:42:31 -0300

    modify css tag ordem_services finnaly.

--- app/helpers/ordem_service_helper.rb
+++ app/helpers/ordem_service_helper.rb
@@ -11,7 +11,7 @@ module OrdemServiceHelper
   def tamanho_letra(qtde_nfe)
     case qtde_nfe
 	    when 1 then tamanho = 170
-	    when 2 then tamanho =  70
+	    when 2 then tamanho =  65
 	  else
 	    	tamanho = 50
     end
