18fe7203600c69f01f104e0770626b6939e6283f
igorcb <igor.batista@gmail.com>
2015-02-10 13:02:03 -0300

    modify css tag ordem_services

--- app/helpers/ordem_service_helper.rb
+++ app/helpers/ordem_service_helper.rb
@@ -8,4 +8,16 @@ module OrdemServiceHelper
   	Time.now.strftime('%Y').to_i - 1
   end
 
+  def tamanho_letra(qtde_nfe)
+    case qtde_nfe
+	    when 1 then tamanho = 200
+	    when 2 then tamanho = 170
+	    when 3 then tamanho = 140
+	    when 4 then tamanho = 110
+	    when 5 then tamanho =  80
+	    else
+	    	tamanho = 50
+    end
+    tamanho
+  end
 end
