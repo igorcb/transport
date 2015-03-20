ceb88064c2433145629b9227aa51bacac2cf3839
igorcb <igor.batista@gmail.com>
2015-03-20 14:39:27 -0300

    spec test sub_cost_center

---
+++ spec/factories/sub_cost_centers.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :sub_cost_center do
+  	cost_center :cost_center
+    descricao "ADMINISTRATIVO"
+  end
+end
--- spec/models/sub_cost_center_spec.rb
+++ spec/models/sub_cost_center_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe SubCostCenter do
-  pending "add some examples to (or delete) #{__FILE__}"
+	let(:cost_center) {FactoryGirl.build(:cost_center)}
+  before do 
+  	#@sub_cost_center = FactoryGirl.build(:cost_center)
+  	@sub_cost_center = cost_center.sub_cost_centers.build(descricao: "MATERIAL DE LIMPEZA")
+  end
+
+  subject {@sub_cost_center}
+
+  it { should respond_to(:cost_center_id) }
+  it { should respond_to(:descricao) }
+
+  it { should respond_to(:cost_center) }
+
 end
