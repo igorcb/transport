324bd861ecc4a8859b2f2352eb4cf9734c399b7a
igorcb <igor.batista@gmail.com>
2015-03-26 15:28:38 -0300

    validates faturar_cada and vencimento_para to clients

--- app/models/client.rb
+++ app/models/client.rb
@@ -11,9 +11,12 @@ class Client < ActiveRecord::Base
   validates :bairro, presence: true, length: { maximum: 100 }
   validates :cidade, presence: true, length: { maximum: 100 }
   validates :estado, presence: true, length: { maximum: 2 }
-
+  
   validates :inscricao_estadual, length: { maximum: 20 }
   validates :inscricao_municipal, length: { maximum: 20 }
+
+  validates :faturar_cada, presence: true, if: :faturar?
+  validates :vencimento_para, presence: true, if: :faturar?
 
   belongs_to :group_client
   has_many :ordem_services
@@ -40,6 +43,10 @@ class Client < ActiveRecord::Base
   	FISICA = 0
   	JURIDICA = 1
   end
+
+  # if pode_faturar?
+  #    self.faturar
+  # end
 
   def self.search_numero(numero)
     #desacentualize(pla_descricao)
