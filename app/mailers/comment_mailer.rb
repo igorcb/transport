3401331611451573822f132e22b54adb299e6aae
igorcb <igor.batista@gmail.com>
2015-03-30 18:32:34 -0300

    add image logo to notification comment.

--- app/mailers/comment_mailer.rb
+++ app/mailers/comment_mailer.rb
@@ -9,7 +9,8 @@ class CommentMailer < ActionMailer::Base
     email = "#{comment.email_destino}, igor.batista@l7logistica.com.br"
     #text_subject = @comments.count < 1 ? "Nova Ocorrência OS. No: #{@ordem_service.id}" : "Nova Interação OS No: #{@ordem_service.id}"
     cte = @ordem_service.cte_keys.present? ? "CT-e: #{@ordem_service.cte_keys.first.cte}" : "CTE: ?"
-    text_subject = "#{cte} / Danfe: #{@ordem_service.danfes} "
+    placa = @ordem_service.ordem_service_logistics.present? ? @ordem_service.ordem_service_logistic.placa : @ordem_service.placa  
+    text_subject = "#{cte} / Danfe: #{@ordem_service.danfes} / Placa: #{placa}"
     mail to: email, bcc: nil, subject: "#{text_subject}"
   end
 
