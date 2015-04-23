3b8601fe07e9fe3b76376125f8496b084134b67f
igorcb <igor.batista@gmail.com>
2015-04-23 12:01:59 -0300

    add validates ordem_service_change

--- app/models/ordem_service_change.rb
+++ app/models/ordem_service_change.rb
@@ -1,4 +1,13 @@
 class OrdemServiceChange < ActiveRecord::Base
   belongs_to :ordem_service
   belongs_to :driver
+
+  validates :source_cep, :source_numero presence: true, :source_complemento, :source_endereco_completo,
+   :source_endereco, :source_cidade, :source_estado
+  validates :target_cep, :target_numero presence: true, :target_complemento, :target_endereco_completo,
+   :target_endereco, :target_cidade, :target_estado
+
+  validates :cubagem, presence: true
+#  validates :valor_declarado, presence: true
+#  validates :valor_total, presence: true  
 end
