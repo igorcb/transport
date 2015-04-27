7620b6b9402305b1d5a3ecd8c80af197b818acb4
igorcb <igor.batista@gmail.com>
2015-04-27 09:30:02 -0300

    modify params[:tipo]

--- app/controllers/ordem_services_controller.rb
+++ app/controllers/ordem_services_controller.rb
@@ -61,7 +61,8 @@ class OrdemServicesController < ApplicationController
   def create
     source_client  = Client.find_by_cpf_cnpj(params[:source_client_cpf_cpnj])
     target_client  = Client.find_by_cpf_cnpj(params[:target_client_cpf_cpnj])
-    if params[:tipo].to_i == OrdemService::TipoOS::MUDANCA
+    puts ">>>>>>>>>>>>>>>>>>>> Tipo: #{params[:ordem_service][:tipo].to_i}"
+    if params[:ordem_service][:tipo].to_i == OrdemService::TipoOS::MUDANCA
       puts ">>>>>>>>>>>>>>>>>>>> Busca por CPF:"
       billing_client = Client.find_by_cpf_cnpj(params[:billing_client_cpf_cnpj])
     else
