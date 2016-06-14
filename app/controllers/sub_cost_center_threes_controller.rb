e9b700373982faa564e4e863830e9ece7dd0a981
Igor C. Batista <igor.batista@gmail.com>
2016-06-14 07:00:45 -0300

    add flash messages

--- app/controllers/sub_cost_center_threes_controller.rb
+++ app/controllers/sub_cost_center_threes_controller.rb
@@ -22,13 +22,27 @@ class SubCostCenterThreesController < ApplicationController
 
   def create
     @sub_cost_center_three = SubCostCenterThree.new(sub_cost_center_three_params)
-    @sub_cost_center_three.save
-    respond_with(@sub_cost_center_three)
+    respond_to do |format|
+      if @sub_cost_center_three.save
+        format.html { redirect_to @sub_cost_center_three, flash: { success: "SubCostCenterThree was successfully created." } }
+        format.json { render action: 'show', status: :created, location: @sub_cost_center_three }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @sub_cost_center_three.errors, status: :unprocessable_entity }
+      end
+    end    
   end
 
   def update
-    @sub_cost_center_three.update(sub_cost_center_three_params)
-    respond_with(@sub_cost_center_three)
+    respond_to do |format|
+      if @sub_cost_center_three.update(sub_cost_center_three_params)
+        format.html { redirect_to @sub_cost_center_three, flash: { success: "SubCostCenterThree was successfully updated." } }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @sub_cost_center_three.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
