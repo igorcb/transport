ebe2888ec56a11f06e223e992c8f81d28b8eeb73
Igor C. Batista <igor.batista@gmail.com>
2017-01-13 08:45:03 -0300

    validations nfs

--- app/models/nfe_key.rb
+++ app/models/nfe_key.rb
@@ -1,6 +1,6 @@
 class NfeKey < ActiveRecord::Base
   validates :nfe, presence: true, length: { maximum: 20 }, numericality: { only_integer: true }
-  validates :chave, numericality: { only_integer: true }, allow_blank: true
+  validates :chave, length: { is: 44 }, numericality: { only_integer: true }, allow_blank: true
 	
 	belongs_to :ordem_service, class_name: "OrdemService", foreign_key: "nfe_id", polymorphic: true, dependent: :destroy
 
