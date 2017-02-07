aea84470fe92284eb10b8b17568e159be2642de7
Igor C. Batista <igor.batista@gmail.com>
2017-02-07 09:49:23 -0300

    search order service by nfs

--- app/controllers/ordem_services_controller.rb
+++ app/controllers/ordem_services_controller.rb
@@ -256,7 +256,7 @@ class OrdemServicesController < ApplicationController
   end
 
   def search_logistic
-    @q = OrdemService.joins(:ordem_service_logistics).paginate(:page => params[:page]).search(params[:query])
+    @q = OrdemService.joins(:ordem_service_logistics).order(id: :desc).paginate(:page => params[:page]).search(params[:query])
     @ordem_services = @q.result
     respond_with(@ordem_services) do |format|
       format.js
