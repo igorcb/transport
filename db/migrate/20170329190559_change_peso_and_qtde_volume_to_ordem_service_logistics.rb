473d0c9f62f15f0c36adcffc4d10701306cd154e
Igor C. Batista <igor.batista@gmail.com>
2017-03-29 16:07:43 -0300

    ordem service change column peso and volume precision 3

---
+++ db/migrate/20170329190559_change_peso_and_qtde_volume_to_ordem_service_logistics.rb
@@ -0,0 +1,14 @@
+class ChangePesoAndQtdeVolumeToOrdemServiceLogistics < ActiveRecord::Migration
+  def self.up
+    change_table :ordem_service_logistics do |t|
+      t.change :peso, :decimal, precision: 10, scale: 3, null: true
+      t.change :qtde_volume, :decimal, precision: 10, scale: 3, null: true
+    end
+  end
+  def self.down
+    change_table :ordem_logistics do |t|
+      t.change :peso, :decimal, precision: 10, scale: 2, null: false
+      t.change :qtde_volume, :decimal, precision: 10, scale: 2, null: false
+    end
+  end
+end
