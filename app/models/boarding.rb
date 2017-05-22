b8808e64edef8610ee14a57b3cc411107c424aaf
Igor C. Batista <igor.batista@gmail.com>
2017-05-22 14:57:17 -0300

    update date shipping when close item boarding

--- app/models/boarding.rb
+++ app/models/boarding.rb
@@ -110,6 +110,7 @@ class Boarding < ActiveRecord::Base
     ActiveRecord::Base.transaction do
       OrdemService.where(id: ordem_service_id).update_all(
                                                       carrier_id: self.carrier_id,
+                                                   date_shipping: self.date_boarding,
                                                           status: OrdemService::TipoStatus::EMBARCADO)
       OrdemServiceLogistic.where(ordem_service_id: ordem_service_id).update_all(delivery_driver_id: self.driver_id)
       Boarding.where(id: self.id).update_all(status: Boarding::TipoStatus::EMBARCADO) if self.check_status_ordem_service?
