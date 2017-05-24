2efd60468e20483ab5c3262e39ce951d9b6ac7c2
Igor C. Batista <igor.batista@gmail.com>
2017-05-24 10:48:53 -0300

    create helper link_to_ordem_service if exist ordem_service

--- app/helpers/input_controls_helper.rb
+++ app/helpers/input_controls_helper.rb
@@ -4,4 +4,26 @@ module InputControlsHelper
     ([["NOTA_FISCAL", 0],["PALETE", 1], ["CINTA", 2], ["CHAPATEX", 3]])
   end
 
+  def link_to_ordem_service(number)
+		nfe_key = NfeKey.where(nfe: number)
+		if nfe_key.present?
+      link_to number, nfe_key.first.ordem_service
+		else
+			number
+		end
+  end
+
 end
+
+  # def alert_color(ordem_service, alert)
+  #   case alert
+  #     when 0 then link_to "Alert", ordem_service, {:target => "_blank"}.merge(class: "btn btn-danger btn-xs")
+  #     when 1 then link_to "Alto" , ordem_service, {:target => "_blank"}.merge(class: "btn btn-warning btn-xs")
+  #     when 2 then link_to "Medio", ordem_service, {:target => "_blank"}.merge(class: "btn btn-yellow btn-xs")
+  #     when 3 then link_to "Baixo", ordem_service, {:target => "_blank"}.merge(class: "btn btn-green btn-xs")
+  #     when 4 then link_to "Normal", ordem_service, {:target => "_blank"}.merge(class: "btn btn-default btn-xs")
+  #   end
+    
+    
+  # end
+
