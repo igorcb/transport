797683271b0e9a8c263bc0663f02319d45d2e3db
Igor C. Batista <igor.batista@gmail.com>
2017-06-14 09:31:43 -0300

    modify volume and weight to 3 decimal precision show and edit

--- db/migrate/20170613194636_modify_valor_peso_to_clients.rb
+++ db/migrate/20170613194636_modify_valor_peso_to_clients.rb
@@ -1,11 +1,13 @@
 class ModifyValorPesoToClients < ActiveRecord::Migration
   def up
-  	change_column :clients, :valor_volume, :decimal, :precision => 10, :scale => 2
-  	change_column :clients, :valor_peso, :decimal, :precision => 10, :scale => 2
+  	change_column :clients, :valor_volume, :decimal, :precision => 10, :scale => 3
+  	change_column :clients, :valor_peso, :decimal, :precision => 10, :scale => 3
+  	change_column :clients, :valor_peso_1500, :decimal, :precision => 10, :scale => 3
   end
 
   def down
-  	change_column :clients, :valor_volume, :decimal, :precision => 10, :scale => 3
-  	change_column :clients, :valor_peso, :decimal, :precision => 10, :scale => 3
+  	change_column :clients, :valor_volume, :decimal, :precision => 10, :scale => 2
+  	change_column :clients, :valor_peso, :decimal, :precision => 10, :scale => 2
+  	change_column :clients, :valor_peso_1500, :decimal, :precision => 10, :scale => 2
   end
 end
