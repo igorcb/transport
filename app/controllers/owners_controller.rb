f51d201eb18eb5b870f412836cf8badad88eaea4
Igor C. Batista <igor.batista@gmail.com>
2017-06-16 15:12:49 -0300

    find owner by cpf_cnpj

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -66,6 +66,10 @@ class OwnersController < ApplicationController
     end
   end
 
+  def get_owner_by_cpf_cnpj
+    
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_owner
