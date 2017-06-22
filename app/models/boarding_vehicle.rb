d2ead2148cdde504211deab4f19b4605e5009fae
Igor C. Batista <igor.batista@gmail.com>
2017-06-22 15:55:13 -0300

    read only false boarding_vehicles

--- app/models/boarding_vehicle.rb
+++ app/models/boarding_vehicle.rb
@@ -2,6 +2,6 @@ class BoardingVehicle < ActiveRecord::Base
   belongs_to :boarding
   belongs_to :vehicle
 
-  default_scope -> { joins(:vehicle).order("vehicles.tipo desc") }
+  default_scope -> { joins(:vehicle).order("vehicles.tipo desc").readonly(false) }
 
 end
