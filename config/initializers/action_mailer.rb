05e5cf539236c2886ceaa7c2faf8d99ecbc78fd0
Igor C. Batista <igor.batista@gmail.com>
2017-06-22 11:59:29 -0300

    config mailer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -28,15 +28,31 @@ if Rails.env.production?
     action_mailer.perform_deliveries = true
     action_mailer.raise_delivery_errors = true
     action_mailer.default :charset => "utf-8"
-    action_mailer.default_url_options = { :host => "www.l7logistica.com.br" }
+    action_mailer.default_url_options = { host:'localhost', port: '3000' }
     action_mailer.smtp_settings = {
       address: ENV['RAILS_MAIL_HOST'],
       port: '587',
       user_name: ENV['RAILS_MAIL_USERNAME'],
       password: ENV['RAILS_MAIL_PASSWORD'],
-      domain: ENV['RAILS_MAIL_DOMAIN'],
+      domain: 'localhost:3000',
       authentication: :plain,
       enable_starttls_auto: true
     }
   end
 end
+
+
+    # action_mailer.delivery_method = :smtp
+    # action_mailer.perform_deliveries = true
+    # action_mailer.raise_delivery_errors = true
+    # action_mailer.default :charset => "utf-8"
+    # action_mailer.default_url_options = { :host => "www.l7logistica.com.br" }
+    # action_mailer.smtp_settings = {
+    #   address: ENV['RAILS_MAIL_HOST'],
+    #   port: '587',
+    #   user_name: ENV['RAILS_MAIL_USERNAME'],
+    #   password: ENV['RAILS_MAIL_PASSWORD'],
+    #   domain: ENV['RAILS_MAIL_DOMAIN'],
+    #   authentication: :plain,
+    #   enable_starttls_auto: true
+    # }
