07575cd6123e98dd803404afa469a469612f07c5
Igor C. Batista <igor.batista@gmail.com>
2017-06-22 14:48:46 -0300

    config action mailer enviroment

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -10,11 +10,11 @@ if Rails.env.development?
     action_mailer.default :charset => "utf-8"
     action_mailer.default_url_options = { host:'localhost', port: '3000' }
     action_mailer.smtp_settings = {
-      address: ENV['RAILS_MAIL_HOST'],
       port: '587',
+      address: ENV['RAILS_MAIL_HOST'],
       user_name: ENV['RAILS_MAIL_USERNAME'],
       password: ENV['RAILS_MAIL_PASSWORD'],
-      domain: 'localhost:3000',
+      domain: ENV['RAILS_MAIL_DOMAIN'],
       authentication: :plain,
       enable_starttls_auto: true
     }
@@ -28,31 +28,15 @@ if Rails.env.production?
     action_mailer.perform_deliveries = true
     action_mailer.raise_delivery_errors = true
     action_mailer.default :charset => "utf-8"
-    action_mailer.default_url_options = { host:'localhost', port: '3000' }
+    action_mailer.default_url_options = { host:'my_host.com.br' }
     action_mailer.smtp_settings = {
-      address: ENV['RAILS_MAIL_HOST'],
       port: '587',
+      address: ENV['RAILS_MAIL_HOST'],
       user_name: ENV['RAILS_MAIL_USERNAME'],
       password: ENV['RAILS_MAIL_PASSWORD'],
-      domain: 'localhost:3000',
+      domain: ENV['RAILS_MAIL_DOMAIN'],
       authentication: :plain,
       enable_starttls_auto: true
     }
   end
 end
-
-
-    # action_mailer.delivery_method = :smtp
-    # action_mailer.perform_deliveries = true
-    # action_mailer.raise_delivery_errors = true
-    # action_mailer.default :charset => "utf-8"
-    # action_mailer.default_url_options = { :host => "www.l7logistica.com.br" }
-    # action_mailer.smtp_settings = {
-    #   address: ENV['RAILS_MAIL_HOST'],
-    #   port: '587',
-    #   user_name: ENV['RAILS_MAIL_USERNAME'],
-    #   password: ENV['RAILS_MAIL_PASSWORD'],
-    #   domain: ENV['RAILS_MAIL_DOMAIN'],
-    #   authentication: :plain,
-    #   enable_starttls_auto: true
-    # }
