9f5f41322412407f825df59e796e413c90c3b78d
Igor C. Batista <igor.batista@gmail.com>
2017-07-11 16:55:30 -0300

    pallets generate ordem_service default true

--- db/migrate/20170711192842_add_generate_ordem_service_to_control_pallets.rb
+++ db/migrate/20170711192842_add_generate_ordem_service_to_control_pallets.rb
@@ -1,5 +1,5 @@
 class AddGenerateOrdemServiceToControlPallets < ActiveRecord::Migration
   def change
-    add_column :control_pallets, :generate_ordem_service, :boolean
+    add_column :control_pallets, :generate_ordem_service, :boolean, default: false
   end
 end
