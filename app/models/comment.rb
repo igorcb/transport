c0d24c06882d0e4546f8d06ec3bcb3d765cc05cc
Igor C. Batista <igor.batista@gmail.com>
2017-10-10 10:33:19 -0300

    comment relationship with input_control.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ActiveRecord::Base
-  belongs_to :ordem_service, class_name: "OrdemService", foreign_key: "comment_id", polymorphic: true, dependent: :destroy
-	belongs_to :boarding, class_name: "Boarding", foreign_key: "comment_id", polymorphic: true, dependent: :destroy
-  belongs_to :input_control, class_name: "InputControl", foreign_key: "comment_id", polymorphic: true, dependent: :destroy
+  belongs_to :boarding, class_name: "Boarding", foreign_key: "comment_id"
+  belongs_to :ordem_service, class_name: "OrdemService", foreign_key: "comment_id"
+  belongs_to :input_control, class_name: "InputControl", foreign_key: "comment_id"
+  
   validates :content, presence: true
-
   
   def type_occurrence_name
     case self.title
@@ -20,9 +20,7 @@ class Comment < ActiveRecord::Base
   end
 
   def send_notification_email(model)
-  	puts ">>>>>>>>>>>>>> send_notification_email. Model: #{model.class}"
   	result = model.class == Boarding
-  	puts ">>>>>>>>>>>>>> result: #{result}"
     case model.class.to_s
       when "OrdemService" then CommentMailer.notification(model).deliver!
       when "Occurrence" then CommentMailer.notification(model).deliver!
@@ -30,12 +28,5 @@ class Comment < ActiveRecord::Base
       when "InputControl" then CommentMailer.notification_input_control(model).deliver!
     end
   end
-
-  def ordem_service
-  	OrdemService.find_by(id: self.comment_id)
-  end
-
-  def boarding
-  	Boarding.find_by(id: self.comment_id)
-  end
 end
+
