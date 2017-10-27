872f1d418c659b97e9680099422e45c8b024a748
Igor C. Batista <igor.batista@gmail.com>
2017-10-27 16:28:45 -0300

    icons status offer driver

--- app/helpers/offer_drivers_helper.rb
+++ app/helpers/offer_drivers_helper.rb
@@ -7,4 +7,14 @@ module OfferDriversHelper
      ["Desistência", 4]
     ])
 	end	
+
+  def status_driver_color(status, status_name)
+    case status
+      when 0 then status_name
+      when 1 then "<i class='fa fa-check' style='color: LimeGreen' aria-hidden='true'></i> #{status_name}".html_safe
+      when 2 then "<i class='fa fa-times' style='color: red' aria-hidden='true'></i> #{status_name}".html_safe
+      when 3 then "<i class='fa fa-square-o' style='color: red' aria-hidden='true'></i> #{status_name}".html_safe
+      when 4 then "<i class='fa fa-minus-square' style='color: orange' aria-hidden='true'></i> #{status_name}".html_safe
+    end
+  end
 end
