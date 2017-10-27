efab0dadde898cc885f0efac92a7d1d2425fa0e7
Igor C. Batista <igor.batista@gmail.com>
2017-10-27 14:46:50 -0300

    forms js offer driver controller

--- app/controllers/offer_drivers_controller.rb
+++ app/controllers/offer_drivers_controller.rb
@@ -22,22 +22,22 @@ class OfferDriversController < ApplicationController
   end
 
   def create
-    @offer_driver = OfferDriver.new(offer_driver_params)
-    @offer_driver.save
-    respond_with(@offer_driver)
+    # @offer_driver = OfferDriver.new(offer_driver_params)
+    # @offer_driver.save
+    # respond_with(@offer_driver)
 
-    #@offer_driver = OfferDriver.new(offer_driver_params)
-    # respond_to do |format|
-    #   if @offer_driver.save
-    #     format.html { redirect_to @offer_driver, flash: { success: "Offer Driver was successfully created." } }
-    #     format.json { render action: 'show', status: :created, location: @offer_driver }
-    #     format.js   { render action: 'show', status: :created, location: @offer_driver }
-    #   else
-    #     format.html { render action: 'new' }
-    #     format.json { render json: @offer_driver.errors, status: :unprocessable_entity }
-    #     format.js   { render json: @offer_driver.errors, status: :unprocessable_entity }
-    #   end
-    # end    
+    @offer_driver = OfferDriver.new(offer_driver_params)
+    respond_to do |format|
+      if @offer_driver.save
+        format.html { redirect_to @offer_driver, flash: { success: "Offer Driver was successfully created." } }
+        format.json { render action: 'show', status: :created, location: @offer_driver }
+        format.js   { render action: 'show', status: :created, location: @offer_driver }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @offer_driver.errors, status: :unprocessable_entity }
+        format.js   { render json: @offer_driver.errors, status: :unprocessable_entity }
+      end
+    end    
   end
 
   def update
