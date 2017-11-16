402fc94f74612b84521f96b51e0ecc1eecf31acb
Igor C. Batista <igor.batista@gmail.com>
2017-11-16 16:24:08 -0300

    search stock equipaments incomplete

--- app/controllers/stock_equipaments_controller.rb
+++ app/controllers/stock_equipaments_controller.rb
@@ -1,4 +1,13 @@
 class StockEquipamentsController < ApplicationController
   def index
+  	@q = ControlPalletInternal.all_equipament.where(id: -1).search(params[:q])
+  end
+
+  def search
+    @q = ControlPalletInternal.all_equipament.search(params[:query])
+    @input_controls = @q.result
+    respond_with(@input_controls) do |format|
+      format.js
+    end
   end
 end
