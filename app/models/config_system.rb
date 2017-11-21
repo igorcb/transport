994c716fc224e239fd8be12ed6d4f990ae794941
Igor C. Batista <igor.batista@gmail.com>
2017-11-21 15:18:41 -0300

    before save key upcase

--- app/models/config_system.rb
+++ app/models/config_system.rb
@@ -1,5 +1,10 @@
 class ConfigSystem < ActiveRecord::Base
-	validates :config_key, presence: true
+	validates :config_key, presence: true, uniqueness: true
 	validates :config_value, presence: true
 	validates :config_description, presence: true
+
+  before_save do |conf| 
+    conf.config_key = config_key.upcase 
+  end
+
 end
