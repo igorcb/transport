0c59799457dfc1b05c48ce9b16394578f1e887d8
Igor C. Batista <igor.batista@gmail.com>
2017-11-30 11:25:55 -0300

    test create tenant execute rakes

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -5,5 +5,13 @@ class Tenant < ActiveRecord::Base
 
 		def create_tenant
 			Apartment::Tenant.create(subdomain)
+			#tasks(subdomain)
+		end
+
+		def tasks(tenant)
+  		load File.join(Rails.root, 'lib', 'tasks', 'config_system.rake')
+			Rake::Task["db:config_system"].invoke(tenant)
 		end
 end
+
+
