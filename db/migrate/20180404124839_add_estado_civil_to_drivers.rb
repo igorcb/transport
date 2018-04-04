a6d2e1ccc7546dbaf27a4b8c2a7657af3e7d3c46
Igor C. Batista <igor.batista@gmail.com>
2018-04-04 11:34:57 -0300

    migration estado civil

---
+++ db/migrate/20180404124839_add_estado_civil_to_drivers.rb
@@ -0,0 +1,7 @@
+class AddEstadoCivilToDrivers < ActiveRecord::Migration
+  def change
+    add_column :drivers, :estado_civil, :integer
+    add_column :drivers, :cor_da_pele, :integer
+    add_column :drivers, :tipo_contrato, :integer
+  end
+end
