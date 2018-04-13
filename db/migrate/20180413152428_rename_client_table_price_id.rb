b34b0232170d53afe543f6e1a5fc036bf67cf5c9
Igor C. Batista <igor.batista@gmail.com>
2018-04-13 12:27:05 -0300

    rename collum ordem service field client table price.

--- db/migrate/20180413152428_rename_client_table_price_id.rb
+++ db/migrate/20180413152428_rename_client_table_price_id.rb
@@ -1,7 +1,7 @@
 class RenameClientTablePriceId < ActiveRecord::Migration
   def change
     change_table :ordem_services do |t|
-      t.rename :client_table_prince_id:, :client_table_price_id:
+      t.rename :client_table_prince_id, :client_table_price_id
     end  	
   end
 end
