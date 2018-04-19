e5524ef209d247d47df447e2cfe33a7f112c455d
Igor C. Batista <igor.batista@gmail.com>
2018-04-19 17:40:04 -0300

    mailer notification account payable send email pluck

--- app/mailers/account_payable_mailer.rb
+++ app/mailers/account_payable_mailer.rb
@@ -5,11 +5,10 @@ class AccountPayableMailer < ActionMailer::Base
     time = Time.zone.now
     @account = account
     if Rails.env.development?
-      email = "igor.batista@gmail.com"  #ENV['RAILS_MAIL_DESTINATION']
+      email = ENV['RAILS_MAIL_DESTINATION']
     end
     if Rails.env.production?
-      #email = "#{comment.email_destino}"
-      email = "igor.batista@gmail.com"  #ENV['RAILS_MAIL_DESTINATION']
+      @account.ordem_service.billing_client.emails.where(sector_id: Sector::TypeSector::FINANCEIRO).pluck(:email)
     end     
     #Assunto do e-mail colocar o numero do container - cliente de destino
     if @account.ordem_service.input_control.present?
@@ -17,31 +16,9 @@ class AccountPayableMailer < ActionMailer::Base
       client_name      = @account.ordem_service.client.nome
     end
     text_subject = "Container: #{number_container} - #{client_name}"
-    puts ">>>>>>>>>>>>>>>>>>>>>>>>>> SendMail: #{text_subject}"
-
-    #attachments[@account.assets.first.asset_file_name] = File.read("#{Rails.root}#{@account.assets.first.asset.path}")
-    #File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
-    
 
     attachments[@account.assets.first.asset_file_name] = File.read(Rails.root.join(@account.assets.first.asset.path)) if File.exist?(Rails.root.join(@account.assets.first.asset.path))
-    # arr = @account.assets.pluck(:asset_file_name)
-    # arr.each do |a|
-    #   puts ">>>>>>>>>>>>>>>>>>>>>> File: #{a.url}"
-    #   #File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
-    #   attachments[a] = File.read("#{Rails.root}#{a.url}")
-    #   #attachments[a] = File.read(a)
-    # end
-    #attachments[] = File.read()
-    # files.each do |file|
-    #   attachments.push = "'#{file.asset_file_name}'"
-    # end  
 
     mail to: email, bcc: nil, subject: "#{text_subject}"
   end
 end
-
-# [09:29, 16/4/2018] Uber Diego Sien: Diego Araújo Gonzaga 
-# Siena preto el 1.0 
-# Och 8323
-# [09:30, 16/4/2018] Uber Diego Sien: Valor tu vê ai o quanto tu acha que pode meter ai
-# [09:31, 16/4/2018] Uber Diego Sien: 15/04 as 5:20 da manha
