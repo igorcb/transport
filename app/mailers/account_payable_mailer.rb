aee319030feaeb17bf4b1d47f7c45052068663df
Igor C. Batista <igor.batista@gmail.com>
2018-04-19 17:29:31 -0300

    mailer notification account payable attachaments[asset_file_name]

--- app/mailers/account_payable_mailer.rb
+++ app/mailers/account_payable_mailer.rb
@@ -23,7 +23,7 @@ class AccountPayableMailer < ActionMailer::Base
     #File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
     
 
-    attachments['report'] = File.read(Rails.root.join(@account.assets.first.asset.path)) if File.exist?(Rails.root.join(@account.assets.first.asset.path))
+    attachments[@account.assets.first.asset_file_name] = File.read(Rails.root.join(@account.assets.first.asset.path)) if File.exist?(Rails.root.join(@account.assets.first.asset.path))
     # arr = @account.assets.pluck(:asset_file_name)
     # arr.each do |a|
     #   puts ">>>>>>>>>>>>>>>>>>>>>> File: #{a.url}"
