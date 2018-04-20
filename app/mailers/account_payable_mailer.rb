213db701aab520de5255321053f046914273b930
Igor C. Batista <igor.batista@gmail.com>
2018-04-20 10:30:57 -0300

    send mail billing_client and source_client

--- app/mailers/account_payable_mailer.rb
+++ app/mailers/account_payable_mailer.rb
@@ -8,14 +8,16 @@ class AccountPayableMailer < ActionMailer::Base
       email = ENV['RAILS_MAIL_DESTINATION']
     end
     if Rails.env.production?
-      email = @account.ordem_service.billing_client.emails.where(sector_id: Sector::TypeSector::FINANCEIRO).pluck(:email)
+      email_billing_client = @account.ordem_service.billing_client.emails.where(sector_id: Sector::TypeSector::FINANCEIRO).pluck(:email)
+      email_target_client  = @account.ordem_service.source_client.emails.where(sector_id: Sector::TypeSector::FINANCEIRO).pluck(:email)
+      email = email_billing_client + email_target_client
     end     
     #Assunto do e-mail colocar o numero do container - cliente de destino
     if @account.ordem_service.input_control.present?
       number_container = @account.ordem_service.input_control.container
       client_name      = @account.ordem_service.client.nome
     end
-    text_subject = "Container: #{number_container} - #{client_name}"
+    text_subject = "COMPROVANTE PGTO - Container: #{number_container} - #{client_name}"
 
     attachments[@account.assets.first.asset_file_name] = File.read(Rails.root.join(@account.assets.first.asset.path)) if File.exist?(Rails.root.join(@account.assets.first.asset.path))
 
