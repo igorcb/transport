34156b743666d09e3305e9f1dfe30cbf967dd7b5
Igor C. Batista <igor.batista@gmail.com>
2018-04-24 15:42:40 -0300

    ordem_service_type_service_freight_weight

---
+++ db/migrate/20180424125352_add_freight_weight_to_ordem_service_table_price.rb
@@ -0,0 +1,6 @@
+class AddFreightWeightToOrdemServiceTablePrice < ActiveRecord::Migration
+  def change
+    add_column :ordem_service_table_prices, :freight_weight_tax, :decimal, precision: 20, scale: 4, default: 0
+    add_column :ordem_service_table_prices, :freight_weight_value, :decimal, precision: 20, scale: 4, default: 0
+  end
+end
