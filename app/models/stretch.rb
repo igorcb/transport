228ae33c13c6bed10e3624b6e90636ebbb3018d3
Igor C. Batista <igor.batista@gmail.com>
2018-05-03 16:15:10 -0300

    validates state and city in Streach

--- app/models/stretch.rb
+++ app/models/stretch.rb
@@ -1,6 +1,6 @@
 class Stretch < ActiveRecord::Base
-	validates :cidade, presence: true, length: { maximum: 20 }
 	validates :estado, presence: true, length: { maximum: 2 }
+  validates :cidade, presence: true, uniqueness: {scope: :estado, case_sensitive: false}
 	validates :destino, presence: true, length: { maximum: 3 }, uniqueness: true
 
 	def description
