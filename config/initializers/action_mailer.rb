8d370eb6d216693b68d829cc8adce945d6bdddb3
Igor C. Batista <igor.batista@gmail.com>
2018-05-25 16:02:22 -0300

    config mailer initializer

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,27 +1,27 @@
 # config/initializers/action_mailer.rb
 
-if Rails.env.development?
-  # Define settings for mailcatcher
-  Rails.application.config.action_mailer.tap do |action_mailer|
-    action_mailer.delivery_method = :smtp
-    action_mailer.perform_deliveries = true
-    action_mailer.logger = true
-    action_mailer.raise_delivery_errors = true
-    action_mailer.default :charset => "utf-8"
-    action_mailer.default_url_options = { host:'localhost', port: '3000' }
-    action_mailer.smtp_settings = {
-      port: '587',
-      address: ENV['RAILS_MAIL_HOST'],
-      user_name: ENV['RAILS_MAIL_USERNAME'],
-      password: ENV['RAILS_MAIL_PASSWORD'],
-      domain: ENV['RAILS_MAIL_DOMAIN'],
-      authentication: :plain,
-      enable_starttls_auto: true
-    }
-  end
-end
+# if Rails.env.development?
+#   # Define settings for mailcatcher
+#   Rails.application.config.action_mailer.tap do |action_mailer|
+#     action_mailer.delivery_method = :smtp
+#     action_mailer.perform_deliveries = true
+#     action_mailer.logger = true
+#     action_mailer.raise_delivery_errors = true
+#     action_mailer.default :charset => "utf-8"
+#     action_mailer.default_url_options = { host:'localhost', port: '3000' }
+#     action_mailer.smtp_settings = {
+#       port: '587',
+#       address: ENV['RAILS_MAIL_HOST'],
+#       user_name: ENV['RAILS_MAIL_USERNAME'],
+#       password: ENV['RAILS_MAIL_PASSWORD'],
+#       domain: ENV['RAILS_MAIL_DOMAIN'],
+#       authentication: :plain,
+#       enable_starttls_auto: true
+#     }
+#   end
+# end
 
-if Rails.env.production?
+# if Rails.env.production?
   # Define settings for sendgrid
   Rails.application.config.action_mailer.tap do |action_mailer|
     action_mailer.delivery_method = :smtp
@@ -39,4 +39,4 @@ if Rails.env.production?
       enable_starttls_auto: true
     }
   end
-end
+# end
