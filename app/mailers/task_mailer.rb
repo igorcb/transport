3775383d9ea5c6ec026aace0ffbda0541b008431
Igor C. Batista <igor.batista@gmail.com>
2018-05-25 16:25:48 -0300

    remove notification_delivery method

--- app/mailers/task_mailer.rb
+++ app/mailers/task_mailer.rb
@@ -1,25 +1,25 @@
 class TaskMailer < ActionMailer::Base
   default from: "sistema@l7logistica.com.br"
 
-  def notification_delivery(task)
-    @task = task
+  # def notification_delivery(task)
+  #   @task = task
 
-    if Rails.env.development?
-      email = ENV['RAILS_MAIL_DESTINATION']
-    end
-    if Rails.env.production?
-      #email = @task.employee.emails.type_sector(Sector::TypeSector::OPERACIONAL).pluck(:email)
-      email_employee = @task.employee.emails.type_sector(Sector::TypeSector::TAREFAS).pluck(:email)
-      email_requester = @task.requester.emails.type_sector(Sector::TypeSector::TAREFAS).pluck(:email)
-      email = email_employee + email_requester
-    end 
+  #   if Rails.env.development?
+  #     email = ENV['RAILS_MAIL_DESTINATION']
+  #   end
+  #   if Rails.env.production?
+  #     #email = @task.employee.emails.type_sector(Sector::TypeSector::OPERACIONAL).pluck(:email)
+  #     email_employee = @task.employee.emails.type_sector(Sector::TypeSector::TAREFAS).pluck(:email)
+  #     email_requester = @task.requester.emails.type_sector(Sector::TypeSector::TAREFAS).pluck(:email)
+  #     email = email_employee + email_requester
+  #   end 
 
-    text_subject = "NOTIFICAÇÃO DE TAREFAS - Task: #{@task.id} "
-    attachments.inline['assinatura_paulo.png'] = File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
+  #   text_subject = "NOTIFICAÇÃO DE TAREFAS - Task: #{@task.id} "
+  #   attachments.inline['assinatura_paulo.png'] = File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
 
-    #send mail
-    mail to: email, bcc: nil, subject: "#{text_subject}"
-  end
+  #   #send mail
+  #   mail to: email, bcc: nil, subject: "#{text_subject}"
+  # end
 
   def notification_employee(task)
     data_email
