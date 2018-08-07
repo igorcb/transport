2832bcb60fbab82ecf862942e9354581bf68caab
Igor C. Batista <igor.batista@gmail.com>
2018-08-07 18:20:54 -0300

    update_or_create_product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	include UpdateOrCreate
+	#include UpdateOrCreate
 
 	validates :descricao, presence: true, length: { maximum: 100 } 
 	validates :category_id, presence: true
@@ -16,5 +16,15 @@ class Product < ActiveRecord::Base
   	  (height / 100 ) * (width / 100) * (length / 100)
     end
   end
+
+	def self.update_or_create(attributes)
+	  assign_or_new(attributes).save
+	end
+
+	def self.assign_or_new(attributes)
+	  obj = first || new
+	  obj.assign_attributes(attributes)
+	  obj
+	end 
 
 end
