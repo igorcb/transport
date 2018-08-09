4df9e132c0dfc64d90ec1378639478969a049cf9
Igor C. Batista <igor.batista@gmail.com>
2018-08-09 16:26:51 -0300

    BigDecimal height_maximum_pallet to_f

--- app/models/product.rb
+++ app/models/product.rb
@@ -20,8 +20,8 @@ class Product < ActiveRecord::Base
   def height_maximum_pallet
   	value = BigDecimal.new(0)
   	if (!height.nil?) || (!layer_pallet.nil?) 
-  		value = (height * layer_pallet) / 100
-   	end
+  		value = (height.to_f * layer_pallet.to_f) / 100
+    end
    	value
   end
 
