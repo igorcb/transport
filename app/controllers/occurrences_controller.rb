f1152247278d4eac78b7f68fe4e50ff1d38ba572
Igor C. Batista <igor.batista@gmail.com>
2018-08-14 09:33:32 -0300

    modify occurrences

--- app/controllers/occurrences_controller.rb
+++ app/controllers/occurrences_controller.rb
@@ -1,41 +1,16 @@
 class OccurrencesController < ApplicationController
   before_filter :authenticate_user!
-  before_action :set_occurrence, only: [:show, :edit, :update, :destroy]
+  before_action :set_occurrence, only: [:show]
   load_and_authorize_resource
 
   respond_to :html
 
   def index
-    @occurrences = Occurrence.all
+    @occurrences = Occurrence.order(date_occurrence: :desc)
     respond_with(@occurrences)
   end
 
   def show
-    @comment = @occurrence.comments.build
-    respond_with(@occurrence)
-  end
-
-  def new
-    @occurrence = Occurrence.new
-    respond_with(@occurrence)
-  end
-
-  def edit
-  end
-
-  def create
-    @occurrence = Occurrence.new(occurrence_params)
-    @occurrence.save
-    respond_with(@occurrence)
-  end
-
-  def update
-    @occurrence.update(occurrence_params)
-    respond_with(@occurrence)
-  end
-
-  def destroy
-    @occurrence.destroy
     respond_with(@occurrence)
   end
 
@@ -44,7 +19,4 @@ class OccurrencesController < ApplicationController
       @occurrence = Occurrence.find(params[:id])
     end
 
-    def occurrence_params
-      params.require(:occurrence).permit(:driver_id, :placa, :cte, :data)
-    end
 end
---
+++ db/migrate/20180813192147_drop_attributes_to_occurrences.rb
@@ -0,0 +1,28 @@
+class DropAttributesToOccurrences < ActiveRecord::Migration
+  def up
+  	remove_index :occurrences, name: :index_occurrences_on_driver_id
+  	remove_column :occurrences, :driver_id
+  	remove_column :occurrences, :placa
+  	remove_column :occurrences, :cte
+  	remove_column :occurrences, :data
+
+		add_column :occurrences, :date_occurrence, :date
+  	add_column :occurrences, :number_nfe, :string
+  	add_column :occurrences, :motive_id, :integer
+  	add_column :occurrences, :observation, :text
+  end
+
+  def down
+  	add_index :occurrences, :driver_id, name: 'index_occurrences_on_driver_id'
+
+  	add_column :occurrences, :driver_id, :integer
+  	add_column :occurrences, :placa, :string
+  	add_column :occurrences, :cte, :string
+  	add_column :occurrences, :data, :date
+
+  	remove_column :occurrences, :number_nfe
+  	remove_column :occurrences, :motive_id
+  	remove_column :occurrences, :observation
+  end
+end
+
