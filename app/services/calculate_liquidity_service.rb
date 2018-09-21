119a8a13ba3ce4aa65300e5546126aaae13c597e
Igor C. Batista <igor.batista@gmail.com>
2018-09-21 16:46:29 -0300

    calculate liquidity add fields quantity_cars

--- app/services/calculate_liquidity_service.rb
+++ app/services/calculate_liquidity_service.rb
@@ -13,6 +13,9 @@ class CalculateLiquidityService
     @lucre = params[:lucre]
     @stretch = StretchRoute.find(params[:trecho_id])
     @insurer = Insurer.find(params[:insurer_id])
+
+    @add_icms_value_frete = params[:add_icms_value_frete]
+    @quantity_cars = params[:quantity_cars]
   end
 
   def call
@@ -32,24 +35,37 @@ class CalculateLiquidityService
 
     pis_cofins = calc_pis_cofins(total_operation.to_f, 7.00)
 
-    total_freight = total_operation.to_f + icms.to_f + pis_cofins.to_f
+    if @add_icms_value_frete == ClientTablePrice::AddIcmsValueFete::SIM
+      icms_value_frete_name = "Incide no valor do frete" 
+      total_freight = total_operation.to_f + icms.to_f + pis_cofins.to_f
+    elsif @add_icms_value_frete == ClientTablePrice::AddIcmsValueFete::NAO
+      icms_value_frete_name = "Não incide no valor do frete" 
+      total_freight = total_operation.to_f + pis_cofins.to_f
+    else #verificar processe que ClientTabelPrice::AddIcmsValueFete::OBEDECE_CLIENTE
+      icms_value_frete_name = "Não incide no valor do frete" 
+      total_freight = total_operation.to_f + pis_cofins.to_f
+    end
+
+    quantity_cars = (@quantity_cars.to_i < 1) ? 1 : @quantity_cars
 
     return {
-           value_nf: @value_nf,
-             weight: @weight,
-            freight: freight,
-         daily_rate: @daily_rate,
-          discharge: discharge,
-             secure: secure,
-       risk_manager: @risk_manager,
-           value_kg: value_per_kg,
-               toll: @toll,
-         total_cost: total_cost,
-              lucre: lucre,
-    total_operation: total_operation,
-         total_icms: icms,
-   total_pis_cofins: pis_cofins,
-      total_freight: total_freight
+             value_nf: @value_nf,
+               weight: @weight,
+              freight: freight,
+           daily_rate: @daily_rate,
+            discharge: discharge,
+               secure: secure,
+         risk_manager: @risk_manager,
+             value_kg: value_per_kg,
+                 toll: @toll,
+ add_icms_value_frete: icms_value_frete_name,
+        quantity_cars: quantity_cars,
+           total_cost: total_cost,
+                lucre: lucre,
+      total_operation: total_operation,
+           total_icms: icms,
+     total_pis_cofins: pis_cofins,
+        total_freight: total_freight
            }
   end
 
