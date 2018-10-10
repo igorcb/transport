2b6684254f1ac67d0e94d92dd9cbd54a4353db8c
Igor C. Batista <igor.batista@gmail.com>
2018-10-10 17:58:33 -0300

    refactoring Notifications unreader

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,25 +1,18 @@
 class NotificationsController < ApplicationController
 	before_filter :authenticate_user!
-	
+
 	def index
-		@notifications = Notification.where(recipient: current_user).unread
+		@notifications = unreader
 	end
 
 	def mark_as_read
-    # if params[:id]
-    #   @notification = Notification.find(params[:id])
-    #   @notification.update_attribute(read_at: Time.zone.now)
-    # else
-    #   @notifications = Notification.where(recipient: current_user).unread.update_all(read_at: Time.zone.now)
-    #   #@notifications.update_all(read_at: Time.zone.now)
-    # end
-
-    # respond_to do |format|
-    #   format.json { render json: { success: true } }
-    # end    
-
-		@notifications = Notification.where(recipient: current_user).unread
+		@notifications = unreader
 		@notifications.update_all(read_at: Time.zone.now)
 		render json: { success: true }
   end	
+
+  private
+    def unreader
+      Notification.where(recipient: current_user).unread
+    end
 end
