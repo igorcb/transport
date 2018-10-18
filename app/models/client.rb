5dac7a55a9d8f9f3ad79f6fbb52b0a5cee3db1f2
Igor C. Batista <igor.batista@gmail.com>
2018-10-18 14:50:51 -0300

    update_or_create client

--- app/models/client.rb
+++ app/models/client.rb
@@ -171,6 +171,16 @@ class Client < ActiveRecord::Base
     ClientTablePrice.where(client_id: self.id, reset: true).first
   end
 
+  def self.update_or_create(attributes)
+    assign_or_new(attributes).save! 
+  end
+
+  def self.assign_or_new(attributes)
+    obj = first || new
+    obj.assign_attributes(attributes)
+    obj
+  end    
+
   private 
     def can_destroy?
       if self.ordem_services.present? || 
