0d190a8421bda705ed93b52353c27a381acc3b90
Igor C. Batista <igor.batista@gmail.com>
2018-11-28 12:50:05 -0300

    adjuste broken company

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,10 +4,10 @@ class Company < ActiveRecord::Base
  #  validates_attachment :image, :presence => true,
  #  										 :content_type => { :content_type => "image/jpg" },
  #  										 :size => { :in => 0..100.kilobytes }
-#	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
-#	has_attached_file :quitter, styles: { small: "64x64", med: "100x100", large: "200x200" }
+	has_attached_file :image, styles: { small: "64x64", med: "100x100", large: "200x200" }
+	has_attached_file :quitter, styles: { small: "64x64", med: "100x100", large: "200x200" }
 
-
+  do_not_validate_attachment_file_type :image, :quitter
 
 	def cidade_estado
     "#{self.cidade} '-' #{self.estado}"
