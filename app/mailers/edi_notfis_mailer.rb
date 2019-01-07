a5d64f42cfa4dc1973592e294bc8af1c78a0fe8a
Igor C. Batista <igor.batista@gmail.com>
2019-01-07 14:44:06 -0300

    modify show notification delivery edi_notfis

--- app/mailers/edi_notfis_mailer.rb
+++ app/mailers/edi_notfis_mailer.rb
@@ -2,10 +2,10 @@ class EdiNotfisMailer < ApplicationMailer
   default from: "sistema@l7logistica.com.br"
 
   def notification(file_name, file, cnpj)
-
+    @date = Date.current.strftime('%d/%m/%Y')
     #email = Client.where(cpf_cnpj: cnpj).first.emails.type_sector(Sector::TypeSector::EDI_NOTFIS_ENTREGA).pluck(:email)*","
     email = 'igor.batista@gmail.com, paulogaldino@l7logistica.com.br'
-    text_subject = "Arquivos Edi NotFis referente ao dia #{Date.current.strftime('%d/%m/%Y')}"
+    text_subject = "Arquivos EDI - OCOREN referente ao dia #{@date}"
 
     #attachments.inline['assinatura_paulo.png'] = File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
     attachments.inline["#{file_name}"] = file
