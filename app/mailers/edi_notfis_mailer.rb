a17da7bf8acd49122f4542db89c4e8f38f43296f
Igor Batista <igor.batista@gmail.com>
2019-01-11 10:04:21 -0300

    mailer the edi_notfis get email group client

--- app/mailers/edi_notfis_mailer.rb
+++ app/mailers/edi_notfis_mailer.rb
@@ -3,8 +3,9 @@ class EdiNotfisMailer < ApplicationMailer
 
   def notification(file_name, file, cnpj)
     @date = Date.current.strftime('%d/%m/%Y')
-    #email = Client.where(cpf_cnpj: cnpj).first.emails.type_sector(Sector::TypeSector::EDI_NOTFIS_ENTREGA).pluck(:email)*","
-    email = 'igor.batista@gmail.com, paulogaldino@l7logistica.com.br'
+    email = 'igor.batista@gmail.com, paulogaldino@l7logistica.com.br, '
+    email += Client.where(cpf_cnpj: cnpj).first.emails.type_sector(Sector::TypeSector::EDI_NOTFIS_ENTREGA).pluck(:email)*","
+
     text_subject = "Arquivos EDI - OCOREN referente ao dia #{@date}"
 
     #attachments.inline['assinatura_paulo.png'] = File.read("#{Rails.root}/app/assets/images/assinatura_paulo.png")
