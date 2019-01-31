4a9708128f57aaba4f2144bfdaf199c4d1f4a9e0
Igor Batista <igor.batista@gmail.com>
2019-01-31 10:44:54 -0300

    get ordem service in nfe_xml through number and key xmls helper field number

--- app/helpers/input_controls_helper.rb
+++ app/helpers/input_controls_helper.rb
@@ -23,7 +23,7 @@ module InputControlsHelper
   def link_to_ordem_service(number)
 		nfe_key = NfeKey.where(nfe_type: 'OrdemService', chave: number)
 		if nfe_key.present?
-      link_to number, nfe_key.first.ordem_service
+      link_to nfe_key.first.number, nfe_key.first.ordem_service
 		else
 			number
 		end
