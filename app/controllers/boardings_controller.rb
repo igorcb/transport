89eb93f54d3b0f4878877747fb02b81c42903faf
Igor Batista <igor.batista@gmail.com>
2019-02-15 15:04:49 -0300

    before start boarding verify if checkin was done...

--- app/controllers/boardings_controller.rb
+++ app/controllers/boardings_controller.rb
@@ -127,7 +127,7 @@ class BoardingsController < ApplicationController
   end
 
   def update_start
-    if Checkin.the_day.input.where(driver_cpf: boarding.driver.cpf).present?
+    if Checkin.the_day.input.where(driver_cpf: @boarding.driver.cpf).present?
       flash[:danger] = "Checkin was not done."
       redirect_to dashboard_oper_path
       return
