c001dab394cf84c5ae05311bdaf6c9f2326d377f
Igor Batista <igor.batista@gmail.com>
2019-03-28 11:00:21 -0300

    carrier_create_or_update_xml check carrier exist

--- app/models/concerns/carrier_create_or_update.rb
+++ app/models/concerns/carrier_create_or_update.rb
@@ -4,22 +4,26 @@ module CarrierCreateOrUpdate
 
   class_methods do
     def carrier_create_or_update_xml(nfe)
-      cnpj_carrier = ''
-      cnpj_carrier = nfe.transp.CNPJ.nil? ? nfe.transp.CPF.to_s : nfe.transp.CNPJ.to_s
-      cnpj_carrier.insert(2, '.').insert(6, '.').insert(10, '/').insert(15, '-')
-      carrier = Carrier.create_with(cnpj: cnpj_carrier,
-                                    nome: nfe.transp.xNome,
-                                fantasia: nfe.transp.xNome,
-                      inscricao_estadual: nfe.transp.IE,
-                     inscricao_municipal: "s/n",
-                                     cep: "s/n",
-                                endereco: nfe.transp.xEnder,
-                                  numero: "s/n",
-                             complemento: nil,
-                                  bairro: "s/n",
-                                  cidade: nfe.transp.xMun,
-                                  estado: nfe.transp.UF).find_or_create_by(cnpj: cnpj_carrier)
-      carrier
+      if nfe.transp.xNome.present?
+        cnpj_carrier = ''
+        cnpj_carrier = nfe.transp.CNPJ.nil? ? nfe.transp.CPF.to_s : nfe.transp.CNPJ.to_s
+        cnpj_carrier.insert(2, '.').insert(6, '.').insert(10, '/').insert(15, '-')
+        carrier = Carrier.create_with(cnpj: cnpj_carrier,
+                                      nome: nfe.transp.xNome,
+                                  fantasia: nfe.transp.xNome,
+                        inscricao_estadual: nfe.transp.IE,
+                       inscricao_municipal: "s/n",
+                                       cep: "s/n",
+                                  endereco: nfe.transp.xEnder,
+                                    numero: "s/n",
+                               complemento: nil,
+                                    bairro: "s/n",
+                                    cidade: nfe.transp.xMun,
+                                    estado: nfe.transp.UF).find_or_create_by(cnpj: cnpj_carrier)
+        carrier
+      else
+        return {success: false, message: "Carrier not registered"}
+      end
     end
 
   end
