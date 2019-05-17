c6cf1ef1fa35bd73f1cd17ac4c6822aa7d6d97f5
Igor Batista <igor.batista@gmail.com>
2019-05-17 16:03:10 -0300

    discharge payment enum type_unit, type_charg,type_calc

--- app/models/discharge_payment.rb
+++ app/models/discharge_payment.rb
@@ -4,5 +4,7 @@ class DischargePayment < ApplicationRecord
   #validates :client_source_id, uniqueness: { scope: [:client_id, :type_unit, :type_charge, :type_calc],
   #                                         message: ": tabela de descagar com tipo de unidade, tipo de carga, tipo de calculo já estão em uso. "}
   validates :type_operation_type, uniqueness: {scope: [:type_operation_id, :type_unit, :type_charge, :type_calc, :price]}
- 
+  enum type_unit: { box: "0", burden: "1", tambo: "2", big_bag: "3"}
+  enum type_charge: { palletized: "0", slam: "1", mixed: "2", adjusted: "3"}
+  enum type_calc: { weight: "0", unit: "1", value: "2"}
 end
