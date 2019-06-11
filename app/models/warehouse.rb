feed9b33db52046ed2ec933c2882ea8196783e87
Thiago <thiagolima86@gmail.com>
2019-06-11 16:38:27 -0300

    implement validation from Warehouse

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,2 +1,6 @@
 class Warehouse < ApplicationRecord
+  validates :name, :district, :state, :city, :zip_code, presence: true
+  validates :address, presence: true, length: { minimum: 5 }
+  validates :number, presence: true, length: { maximum: 10 }
+
 end
