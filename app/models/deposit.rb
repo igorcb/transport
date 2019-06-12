ba2beb142af19d86d1275c5d3d59093d76eb5f8a
Thiago <thiagolima86@gmail.com>
2019-06-12 11:35:08 -0300

    validade with scope

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,5 +1,6 @@
 class Deposit < ApplicationRecord
   belongs_to :warehouse
 
-  validates :warehouse, :name, presence: true
+  validates :warehouse, presence: true
+  validates :name, presence: true, uniqueness: { scope: :warehouse, message: "should happen once per warehouse", case_sensitive: false }
 end
