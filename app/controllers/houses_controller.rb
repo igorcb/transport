b0fc65cee4602d740c86bd8a29c858c6ba7fc478
Thiago <thiagolima86@gmail.com>
2019-06-21 17:22:41 -0300

    Change House add field actived and occupied

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -79,6 +79,6 @@ class HousesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def house_params
-      params.require(:house).permit(:name, :floor_id)
+      params.require(:house).permit(:name, :occupied, :actived, :floor_id)
     end
 end
