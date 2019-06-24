462af6f2e1fd7e75ac0b5dc635ee5bb7f05f291a
Igor Batista <igor.batista@gmail.com>
2019-06-24 11:07:07 -0300

    add and remove with js annt_vehicles to antt

--- app/controllers/antts_vehicles_controller.rb
+++ app/controllers/antts_vehicles_controller.rb
@@ -1,4 +1,5 @@
 class AnttsVehiclesController < ApplicationController
+	respond_to :js
 	def new
 		@antts_vehicles = AnttsVehicles.new
 	end
@@ -14,19 +15,27 @@ class AnttsVehiclesController < ApplicationController
   	@antts_vehicles = AnttsVehicles.new
   	if @vehicle.blank?
       @antts_vehicles.errors.add("AnttsVehicles", "Vehicle is not present.")
-      return  		
+      return
   	end
 
   	@antts_vehicles = AnttsVehicles.new(antts_vehicles_params)
 		@antts_vehicles.vehicle_id = @vehicle.id if @vehicle.present?
 		if @antts_vehicles.save
-  	  flash.now[:notice] = "AnttsVehicles add successfully." 
+  	  flash.now[:notice] = "AnttsVehicles add successfully."
     else
       @antts_vehicles.errors.full_messages.each do |msg|
-        flash[:error] = msg  
+        flash[:error] = msg
       end
 	  end
   end
+
+	def destroy
+		@antts_vehicles = AnttsVehicles.find(params[:id])
+		@antt = @antts_vehicles.antt
+		@antts_vehicles_id = params[:id]
+    @antts_vehicles.destroy
+		respond_with(@antt)
+	end
 
   private
 
