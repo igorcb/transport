e7ae4120684acf8077ca694642a470d434688e37
Thiago <thiagolima86@gmail.com>
2019-06-25 09:08:47 -0300

    add has_many in warehouse

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,4 +1,7 @@
 class Warehouse < ApplicationRecord
+  has_many :deposit
+  has_many :street , through: :deposit
+  
   validates :name, :district, :state, :city, :zip_code, presence: true
   validates :address, presence: true, length: { minimum: 5 }
   validates :number, presence: true, length: { maximum: 10 }
