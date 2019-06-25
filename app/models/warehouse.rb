27cbe36ba67b6b49db5252b72c1961610e24251e
Thiago <thiagolima86@gmail.com>
2019-06-25 10:07:20 -0300

    correction has_many to plural in warehouse

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -1,7 +1,7 @@
 class Warehouse < ApplicationRecord
   has_many :deposit
-  has_many :street , through: :deposit
-  
+  has_many :streets , through: :deposits
+
   validates :name, :district, :state, :city, :zip_code, presence: true
   validates :address, presence: true, length: { minimum: 5 }
   validates :number, presence: true, length: { maximum: 10 }
