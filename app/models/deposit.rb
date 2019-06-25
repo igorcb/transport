93ee0f306b5883fbce84b376ebf3436ff7e957a0
Thiago <thiagolima86@gmail.com>
2019-06-25 09:14:32 -0300

    add has_many in deposit

--- app/models/deposit.rb
+++ app/models/deposit.rb
@@ -1,5 +1,6 @@
 class Deposit < ApplicationRecord
   belongs_to :warehouse
+  has_many :street
 
   validates :warehouse, presence: true
   validates :name, presence: true, uniqueness: { scope: :warehouse, message: "should happen once per warehouse", case_sensitive: false }
