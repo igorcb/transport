1790a9d51b7394a1266d5e6039e7349ee45652b9
Igor Batista <igor.batista@gmail.com>
2019-06-26 18:09:29 -0300

    new type file_edi

--- spec/factories/file_edi.rb
+++ spec/factories/file_edi.rb
@@ -1,6 +1,21 @@
 require_relative '../place_helper'
 
 FactoryBot.define do
+  factory :file_edi, class: 'FileEdi' do
+    type_file {FileEdi::TypeFile::EDI_OCCURRENCE}
+    date_file { Date.current }
+    name_file { "#{Time.now}.txt" }
+    content { Faker::Lorem.sentences(1) }
+    date_boarding { Date.current }
+    place { PlaceHelper.faker_place }
+    weight {Faker::Number.decimal(2, 3)}
+    volume {Faker::Number.number(5)}
+    value_total {Faker::Number.decimal(2, 3)}
+    qtde {Faker::Number.number(2)}
+    shipper
+    carrier
+  end
+
   factory :file_edi_occurrence, class: 'FileEdi' do
     type_file {FileEdi::TypeFile::EDI_OCCURRENCE}
     date_file { Date.current }
