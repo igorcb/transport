875314306638c9853c0dccd6d2f871102209e1d0
Igor Batista <igor.batista@gmail.com>
2019-07-02 11:14:33 -0300

    search nfe_xml by key

--- app/models/nfe_xml.rb
+++ app/models/nfe_xml.rb
@@ -81,7 +81,7 @@ class NfeXml < ActiveRecord::Base
   end
 
 	def self.ransackable_attributes(auth_object = nil)
-		['id', 'issue_date', 'numero']
+		['id', 'issue_date', 'numero', 'chave']
 	end
   def status_os_create
     case self.create_os
