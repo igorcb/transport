4ef2ccbf996a3158b126e2a0e14a955d4d14e175
Thiago <thiagolima86@gmail.com>
2019-07-16 17:37:22 -0300

    count number js to dashboard sup

--- app/assets/javascripts/counter_number.js
+++ app/assets/javascripts/counter_number.js
@@ -1,38 +1,25 @@
-class CounterNumber {
 
-  constructor(selector){
-    this.elem = document.querySelector(selector)
-    return this.animateNumber()
-  }
+$(document).ready(function() {
+  // elem = $(".counter_number")
+  // animateNumber()
+});
 
-  getInitNumber(){
-    return this.elem.getAttribute("data-init")
-  }
-
-  getEndNumber(){
-    return this.elem.getAttribute("data-end")
-  }
-
-  animateNumber(){
-    var init = this.getInitNumber()
-    var el = this.elem
-    var end = this.getEndNumber()
+  function animateNumber(elem){
+    var init = elem.attr("data-init")
+    var el = elem
+    var end = elem.attr("data-end")
 
     if (end >= init) {
-    var dif = end-init
-    var speed = (dif < 50) ? 60 : 0.1;
-    console.log(speed);
-    var loop =  setInterval( function(){
-          el.innerHTML = init
-          if (init >= end) {
-            clearInterval(loop);
-          }
-          init++
-      }, speed);
+      var dif = end-init
+      var speed = (dif < 50) ? 60 : 0.1;
+      console.log(speed);
+      var loop =  setInterval( function(){
+            el.html(init)
+            if (init >= end) {
+              clearInterval(loop);
+            }
+            init++
+        }, speed);
 
     }
   }
-
-
-
-}
