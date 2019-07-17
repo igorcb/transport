1b7ec44605cf8307f772968aa8f4f0d3cb579cfb
Thiago <thiagolima86@gmail.com>
2019-07-17 14:49:52 -0300

    Layout dashboard

---
+++ app/assets/javascripts/counter_number.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // elem = $(".counter_number")
+  // animateNumber()
+});
+
+  function counterNumber(elem){
+    var init = elem.attr("data-init")
+    var el = elem
+    var end = elem.attr("data-end")
+
+    if (end >= init) {
+      var dif = end-init
+      var speed = (dif < 50) ? 60 : 0.1;
+      console.log(speed);
+      var loop =  setInterval( function(){
+            el.html(init)
+            if (init >= end) {
+              clearInterval(loop);
+            }
+            init++
+        }, speed);
+
+    }
+  }
