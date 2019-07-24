0b2f640dc396634a6a68c983c999c780e0513276
Igor Batista <igor.batista@gmail.com>
2019-07-24 15:05:15 -0300

    param time_zone in dashboard_admin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,12 +11,12 @@ class StaticPagesController < ApplicationController
 
 	def dashboard_admin
 		last_day = 30
-		@inputs_daily = InputControl.where("input_controls.date_scheduled > ?", Time.now-last_day.days).group_by_day(:date_scheduled).count
-		@carriers = InputControl.joins(:carrier).where("input_controls.created_at > ?", Time.now-last_day.days).select("carriers.nome").group("carriers.nome").count(:id)
-		@input_weight = InputControl.where("input_controls.date_scheduled > ?", Time.now-last_day.days).sum(:weight)
-		@input_volume = InputControl.where("input_controls.date_scheduled > ?", Time.now-last_day.days).sum(:volume)
-		@inputs_num = InputControl.where("input_controls.date_scheduled > ?", Time.now-last_day.days).count
-		@boardings_num = Boarding.where("date_boarding > ?", Time.now-last_day.days).count
+		@inputs_daily = InputControl.where("input_controls.date_scheduled > ?", Date.current - last_day.days).group_by_day(:date_scheduled, time_zone: false).count
+		@carriers = InputControl.joins(:carrier).where("input_controls.created_at > ?", Date.current - last_day.days).select("carriers.nome").group("carriers.nome").count(:id)
+		@input_weight = InputControl.where("input_controls.date_scheduled > ?", Date.current - last_day.days).sum(:weight)
+		@input_volume = InputControl.where("input_controls.date_scheduled > ?", Date.current - last_day.days).sum(:volume)
+		@inputs_num = InputControl.where("input_controls.date_scheduled > ?", Date.current - last_day.days).count
+		@boardings_num = Boarding.where("date_boarding > ?",Date.current - last_day.days).count
 	end
 
 	def dashboard_visit
