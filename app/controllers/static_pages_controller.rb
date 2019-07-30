145db7aff0aebe792cff4b9d605e3bf3ce239ddc
Thiago <thiagolima86@gmail.com>
2019-07-30 12:13:57 -0300

    convert dash board admin input daily in month

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,14 +11,14 @@ class StaticPagesController < ApplicationController
 
 	def dashboard_admin
 		last_day = 30
-		@inputs_daily = dashboard_date(InputControl.where("date_scheduled > ?", Date.current - last_day.days)
-												.group_by_day(:date_scheduled, time_zone: false)
+		@inputs_month = dashboard_date(InputControl.where("date_scheduled > ?", Date.current - 360.days)
+												.group_by_month(:date_scheduled, time_zone: false)
 												.count,
-												"%d %b")
-		@boardings_daily = dashboard_date(Boarding.where("created_at > ?", Date.current - last_day.days)
-												.group_by_day(:created_at, time_zone: false)
+												"%b %Y")
+		@boardings_month = dashboard_date(Boarding.where("created_at > ?", Date.current - 360.days)
+												.group_by_month(:created_at, time_zone: false)
 												.count,
-												"%d %b")
+												"%b %Y")
 		@carriers = InputControl.joins(:carrier).where("input_controls.created_at > ?", Date.current - last_day.days)
 														.select("carriers.fantasia")
 														.group("carriers.fantasia")
@@ -28,7 +28,6 @@ class StaticPagesController < ApplicationController
 		# parte do input status
 		@input_status = InputControl.where("date_scheduled > ?", Date.current - last_day.days).group("date_scheduled")
 		@boarding_status = Boarding.where("created_at > ?", Date.current - last_day.days).group("created_at")
-		# @ordem_services_status = OrdemService.where("data > ?", Date.current - 30.days).group(:status).count
 
 		@input_weight = InputControl.where("date_scheduled > ?", Date.current - last_day.days).sum(:weight)
 		@input_volume = InputControl.where("date_scheduled > ?", Date.current - last_day.days).sum(:volume)
