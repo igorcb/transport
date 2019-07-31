6a8bc864bb244dd590dc4b426516e64dfa5691f5
Thiago <thiagolima86@gmail.com>
2019-07-31 09:48:29 -0300

    Palletes of Nfe Xml

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,24 +11,14 @@ class StaticPagesController < ApplicationController
 
 	def dashboard_admin
 		last_day = 30
-		@inputs_month = dashboard_date(InputControl.where("date_scheduled > ?", Date.current - 18.months)
-												.group_by_month(:date_scheduled, time_zone: false)
-												.count,
-												"%b %Y")
-		@boardings_month = dashboard_date(Boarding.where("created_at > ?", Date.current - 18.month)
-												.group_by_month(:created_at, time_zone: false)
-												.count,
-												"%b %Y")
-
-		@inputs_daily = dashboard_date(InputControl.where("date_scheduled > ?", Date.current - last_day.day)
+		@inputs_daily = dashboard_date(InputControl.where("date_scheduled > ?", Date.current - last_day.days)
 												.group_by_day(:date_scheduled, time_zone: false)
 												.count,
 												"%d %b")
-		@boardings_daily = dashboard_date(Boarding.where("created_at > ?", Date.current - last_day.day)
+		@boardings_daily = dashboard_date(Boarding.where("created_at > ?", Date.current - last_day.days)
 												.group_by_day(:created_at, time_zone: false)
 												.count,
 												"%d %b")
-
 		@carriers = InputControl.joins(:carrier).where("input_controls.created_at > ?", Date.current - last_day.days)
 														.select("carriers.fantasia")
 														.group("carriers.fantasia")
@@ -38,6 +28,7 @@ class StaticPagesController < ApplicationController
 		# parte do input status
 		@input_status = InputControl.where("date_scheduled > ?", Date.current - last_day.days).group("date_scheduled")
 		@boarding_status = Boarding.where("created_at > ?", Date.current - last_day.days).group("created_at")
+		# @ordem_services_status = OrdemService.where("data > ?", Date.current - 30.days).group(:status).count
 
 		@input_weight = InputControl.where("date_scheduled > ?", Date.current - last_day.days).sum(:weight)
 		@input_volume = InputControl.where("date_scheduled > ?", Date.current - last_day.days).sum(:volume)
@@ -49,6 +40,7 @@ class StaticPagesController < ApplicationController
 
     @boarding_weight = Boarding.joins(:nfe_keys).where("date_boarding > ?", Date.current - 30.days).sum("nfe_keys.peso")
 		@boarding_volume = Boarding.joins(:nfe_keys).where("date_boarding > ?", Date.current - 30.days).sum("nfe_keys.volume")
+		@pallets_of_nfe_xml = NfeXml.where("created_at < ? and created_at >= ?", Date.current, Date.current - 120.day).group_by_day(:created_at).sum(:qtde_pallet)
 		# @next_ordem_service_peso = OrdemService.where("data > ? and data <= ?", Date.current, Date.current + 3.day).group(:data).sum(:peso)
 	end
 
