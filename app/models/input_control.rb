e51d62bab230b0f5e8fb06a683d99abf4605ccfc
Igor Batista <igor.batista@gmail.com>
2019-08-23 10:58:04 -0300

    input_control test

--- app/models/input_control.rb
+++ app/models/input_control.rb
@@ -630,6 +630,7 @@ class InputControl < ActiveRecord::Base
           nfe = NFe::NotaFiscal.new.load_xml_serealize(file)
           NfeXml.processado.is_not_input.where(chave: xml).update_all(nfe_type: "InputControl", nfe_id: input_control.id)
           NfeXml.product_create_or_update_xml('input_controls', input_control, nfe_xml, nfe)
+          NfeXml.where(id: nfe_xml.id).update_all(qtde_pallet: nfe_xml.item_input_controls.sum(:qtde_pallet))
           InputControls::SetWeightAndVolumeService.new(input_control).call
         end
         return {success: true, message: "XML adicionado na remessa de entrada #{input_control.id} com sucesso."}
