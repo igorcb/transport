196c3c32e2b170dd01093caafbc91c77b57eee00
Igor Batista <igor.batista@gmail.com>
2019-09-19 11:12:56 -0300

    finish boarding generate service

--- app/services/boardings/generate_service.rb
+++ app/services/boardings/generate_service.rb
@@ -9,19 +9,21 @@ module Boardings
     def call
       #byebug
       return {success: false, message: "Ordem Service not selected."} if @ordem_service_ids.blank?
-      # return {success: false, message: "Boarding has payment discharge."} if @boarding.account_payables.present?
-      # return {success: false, message: "O.S. does not exist."} if @ordem_service.nil?
-      # return {success: false, message: "O.S. does not have the status OPEN."} if @ordem_service.status != OrdemService::TipoStatus::ABERTO
-      # return {success: false, message: "O.S. be duplicated on boarding."} if @boarding.boarding_items.where(ordem_service_id: @ordem_service.id).present?
+      return {success: false, message: "Default driver key, not set."} if !driver_not_information?
+      return {success: false, message: "Default carrier key, not set."} if !carrier_not_information?
+      return {success: false, message: "Selected work ordem_service must be in status OPEN."} if !status_open?(@ordem_service_ids)
+      
+      driver = Driver.where(id: ConfigSystem.where(config_key: 'DRIVER_DEFAULT').first.config_value).first
+      carrier = Carrier.where(id: ConfigSystem.where(config_key: 'CARRIER_DEFAULT').first.config_value).first
 
       begin
         ActiveRecord::Base.transaction do
-          boarding = Boarding.create(driver_id: driver, carrier_id: carrier, status: Boarding::TipoStatus::ABERTO)
-          hash_ids.each do |os|
-          	boarding.boarding_items.create!(ordem_service_id: os, delivery_number: 1)
+          boarding = Boarding.create!(driver: driver, carrier: carrier, status: Boarding::TipoStatus::ABERTO)
+          @ordem_service_ids.each do |ordem_service|
+          	boarding.boarding_items.create!(ordem_service_id: ordem_service, delivery_number: 1)
           end
-          OrdemService.where(id: hash_ids).update_all(status: OrdemService::TipoStatus::AGUARDANDO_EMBARQUE)
-          return {success: true, message: "Boarding Items created successfully."}
+          OrdemService.where(id: @ordem_service_ids).update_all(status: OrdemService::TipoStatus::AGUARDANDO_EMBARQUE)
+          return {success: true, message: "Boarding created successfully."}
         end
       rescue => e
         return {success: false, message: e.message}
@@ -29,5 +31,38 @@ module Boardings
     end
 
     private
+
+      def driver_not_information?
+        result = true
+        conf = ConfigSystem.where(config_key: 'DRIVER_DEFAULT').first
+        if conf.present?
+          conf.config_value.to_i
+          return false if !Driver.where(id: conf.config_value.to_i).present?
+        else
+          return false
+        end
+        result = true
+      end
+
+      def carrier_not_information?
+        result = true
+        conf = ConfigSystem.where(config_key: 'CARRIER_DEFAULT').first
+        if conf.present?
+          conf.config_value.to_i
+          return false if !Carrier.where(id: conf.config_value.to_i).present?
+        else
+          return false
+        end
+        result = true
+      end
+
+      def status_open?(ordem_service_ids)
+        positivo = true
+        ordem_service_ids.each do |id|
+          ordem_service = OrdemService.where(id: id).first
+          return false if ordem_service.status != OrdemService::TipoStatus::ABERTO
+        end
+        positivo
+      end
   end
 end
--- spec/services/boardings/generate_service_spec.rb
+++ spec/services/boardings/generate_service_spec.rb
@@ -9,19 +9,46 @@ RSpec.describe Boardings::GenerateService, type: :service do
       @ordem_service_first = FactoryBot.create(:ordem_service)
       @ordem_service_second = FactoryBot.create(:ordem_service)
       @ordem_service_third = FactoryBot.create(:ordem_service)
-      @ordem_service_ids = [@ordem_service_first.id, @ordem_service_second.id, @ordem_service_ids.id]
+      @ordem_service_ids = [@ordem_service_first.id, @ordem_service_second.id, @ordem_service_third.id]
     end
 
-    it "when there is no ordem service" do
+    it "when ordem service is not exist" do
       result = Boardings::GenerateService.new(nil).call
       expect(result[:success]).to be_falsey
       expect(result[:message]).to match("Ordem Service not selected.")
     end
 
-    it "when exist ordem service" do
+    it "when the default driver key and driver does not exist" do
+      ConfigSystem.where(config_key: 'DRIVER_DEFAULT').destroy_all
       result = Boardings::GenerateService.new(@ordem_service_ids).call
       expect(result[:success]).to be_falsey
-      expect(result[:message]).to match("Ordem Service not selected.")
+      expect(result[:message]).to match("Default driver key, not set.")
+    end
+
+    it "when the default carrier key and carrier does not exist" do
+      ConfigSystem.where(config_key: 'CARRIER_DEFAULT').destroy_all
+      result = Boardings::GenerateService.new(@ordem_service_ids).call
+      expect(result[:success]).to be_falsey
+      expect(result[:message]).to match("Default driver key, not set.")
+    end
+
+    it "when the ordem_service is not as OPEN" do
+      ConfigSystem.create_with(config_key: "DRIVER_DEFAULT", config_value: "1", config_description: "Motorista padrão do sistema").find_or_create_by(config_key: "DRIVER_DEFAULT")
+      ConfigSystem.create_with(config_key: "CARRIER_DEFAULT", config_value: "1", config_description: "Transportadora padrão do sistema").find_or_create_by(config_key: "CARRIER_DEFAULT")
+      OrdemService.where(id: [@ordem_service_ids]).update_all(status: OrdemService::TipoStatus::AGUARDANDO_EMBARQUE)
+      result = Boardings::GenerateService.new(@ordem_service_ids).call
+      expect(result[:success]).to be_falsey
+      expect(result[:message]).to match("Selected work ordem_service must be in status OPEN.")
+    end
+
+    it "when exist ordem service" do
+      ConfigSystem.create_with(config_key: "DRIVER_DEFAULT", config_value: "1", config_description: "Motorista padrão do sistema").find_or_create_by(config_key: "DRIVER_DEFAULT")
+      ConfigSystem.create_with(config_key: "CARRIER_DEFAULT", config_value: "1", config_description: "Transportadora padrão do sistema").find_or_create_by(config_key: "CARRIER_DEFAULT")
+      BoardingItem.where(ordem_service_id: [@ordem_service_ids]).destroy_all
+      OrdemService.where(id: [@ordem_service_ids]).update_all(status: OrdemService::TipoStatus::ABERTO)
+      result = Boardings::GenerateService.new(@ordem_service_ids).call
+      expect(result[:success]).to be_truthy
+      expect(result[:message]).to match("Boarding created successfully.")
     end
 
   end
