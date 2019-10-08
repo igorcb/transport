3b2a7b2dc2b09771366f82aef9b91d70493f9499
Igor Batista <igor.batista@gmail.com>
2019-10-08 10:06:46 -0300

    add button add_item when product not exist in conference

--- app/helpers/conference_items_helper.rb
+++ app/helpers/conference_items_helper.rb
@@ -1,2 +1,7 @@
 module ConferenceItemsHelper
+  def location_item_conference(conference_id, product_id)
+    conference_items = ConferenceItem.where(conference_id:conference_id, product_id: product_id).first
+    #conference_items.id if conference_items.present?
+    result = conference_items.present? ? conference_items.id : ""
+  end
 end
