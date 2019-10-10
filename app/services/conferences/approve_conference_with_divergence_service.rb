9b9d3230cbfdb80420536c9c4f81fd1b01252201
Igor Batista <igor.batista@gmail.com>
2019-10-10 15:14:51 -0300

    correct approved conferencw with divergence

--- app/services/conferences/approve_conference_with_divergence_service.rb
+++ app/services/conferences/approve_conference_with_divergence_service.rb
@@ -14,22 +14,24 @@ module Conferences
       @conference_items = @conference.conference_items
 
       begin
-        # byebug
+        #byebug
         ActiveRecord::Base.transaction do
           @divergencias = []
+
           if @item_input_controls.count < @conference_items.count
             @conference_items.each do |conference_item|
               qtde_nfe = @item_input_controls[conference_item.product_id].nil? ? 0 : @item_input_controls[conference_item.product_id]
               qtde_oper = conference_item.qtde_oper
-              item_input_control  = @input_control.item_input_controls.where(product_id: conference_item.product_id).first
-              number_nfe = item_input_control.nil? ? nil : item_input_control.number_nfe
+              product = Product.where(id: conference_item.product_id).first
+              item_input = @input_control.item_input_controls.where(product_id: conference_item.product_id).first
+              nfe_id = item_input.nil? ? nil : item_input.nfe_xml_id
 
               if qtde_nfe < qtde_oper
                 sobra = qtde_oper - qtde_nfe
-                @divergencias.push({nfe_xml_id: number_nfe, product_id: conference_item.product_id,  sobras: sobra, type_breakdown: 1, unid_medida: "CX"})
+                @divergencias.push({nfe_xml_id: nfe_id, product_id: conference_item.product_id, sobras: sobra, unid_medida: product.unid_medida, type_breakdown: Breakdown::TypeBreakdown::PRIMEIRA_PERNA_TRANSP})
               elsif qtde_nfe > qtde_oper
                 falta = qtde_nfe - qtde_oper
-                @divergencias.push({nfe_xml_id: number_nfe, product_id: conference_item.product_id,  faltas: falta, type_breakdown: 1, unid_medida: "CX"})
+                @divergencias.push({nfe_xml_id: nfe_id, product_id: conference_item.product_id, faltas: falta, unid_medida: product.unid_medida, type_breakdown: Breakdown::TypeBreakdown::PRIMEIRA_PERNA_TRANSP})
               end
             end
           else
@@ -38,29 +40,39 @@ module Conferences
               item = @conference_items.where(product_id: item_input_control[0]).first
               item = item.nil? ? 0 : item
               item_input  = @input_control.item_input_controls.where(product_id: item_input_control[0]).first
-              number_nfe = item_input.nil? ? nil : item_input.number_nfe
+              nfe_id = item_input.nil? ? nil : item_input.nfe_xml_id
 
               if qtde_nfe < item.qtde_oper
                 sobra = item.qtde_oper - qtde_nfe
-                @divergencias.push({nfe_xml_id: number_nfe, product_id: conference_item.product_id,  sobras: sobra, type_breakdown: 1, unid_medida: "CX"})
+                @divergencias.push({nfe_xml_id: nfe_id, product_id: item.product_id, sobras: sobra, unid_medida: item_input.unid_medida, type_breakdown: Breakdown::TypeBreakdown::PRIMEIRA_PERNA_TRANSP})
               elsif qtde_nfe > item.qtde_oper
                 falta = qtde_nfe - item.qtde_oper
-                @divergencias.push({nfe_xml_id: number_nfe, product_id: conference_item.product_id,  faltas: falta, type_breakdown: 1, unid_medida: "CX"})
+                @divergencias.push({nfe_xml_id: nfe_id, product_id: item.product_id, faltas: falta, unid_medida: item_input.unid_medida, type_breakdown: Breakdown::TypeBreakdown::PRIMEIRA_PERNA_TRANSP})
               end
             end
           end
+
           @divergencias.each do |divergencia|
-            #@input_control.breakdowns.create!(divergencia)
             Breakdown.where(breakdown_type: "InputControl", breakdown_id: @input_control, product_id: divergencia[:product_id] ).update_or_create(divergencia)
           end
           @conference.update_attributes(approved: :yes, status: :finish)
-          #@input_control.update_attributes(status: InputControl::TipoStatus::)
+
+          return {success: true, message: "Breakdown on input_control created successfully."}
         end
-        return {success: true, message: "Breakdown on input_control created successfully."}
+
       rescue => e
         return {success: false, message: e.message}
       end
     end
+
+    private
+        def method
+          #code
+        end
+
+        def method
+          #code
+        end
   end
 
 end
