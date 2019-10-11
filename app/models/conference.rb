175cca549fe560e207ee559b3705208c15a09d6e
Igor Batista <igor.batista@gmail.com>
2019-10-11 09:48:59 -0300

    dependet destroy conference items

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,7 +1,7 @@
 class Conference < ApplicationRecord
   belongs_to :user
   belongs_to :input_control, class_name: "InputControl", foreign_key: "conference_id", polymorphic: true, dependent: :destroy, required: false
-  has_many :conference_items
+  has_many :conference_items, dependent: :destroy
 
   enum type_operation: [:input_control, :boarding]
   enum status: {start: "0", finish: "1"}
