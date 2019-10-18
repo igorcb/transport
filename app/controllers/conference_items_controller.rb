5e6ddf55148a864e1ebbd5fce9079e31e961a79a
Thiago Lima <thiagolima86@gmail.com>
2019-10-18 14:32:44 -0300

    icon check when correction button was correction yet

--- app/controllers/conference_items_controller.rb
+++ app/controllers/conference_items_controller.rb
@@ -11,7 +11,7 @@ class ConferenceItemsController < ApplicationController
     @input_control = InputControl.find(params[:input_control_id])
     @conference = @input_control.conferences.last
     @conference_item = ConferenceItem.where(conference_id: @conference, product_id: params[:id]).first
-    @conference_item.update_attributes(qtde_oper: params[:qtde_oper])
+    @conference_item.update_attributes(qtde_oper: params[:qtde_oper], qtde_sup: params[:qtde_oper])
 
     redirect_to review_conference_input_control_path(params[:input_control_id])
   end
--- app/controllers/conferences_controller.rb
+++ app/controllers/conferences_controller.rb
@@ -25,27 +25,12 @@ class ConferencesController < ApplicationController
     redirect_to items_input_control_path(@input_control)
   end
 
-  # def add_avaria
-  #   input_control = InputControl.where(id: params[:id]).first
-  #   product = Product.find(params[:product_id])
-  #
-  #   Conferences::ConferenceItemBreakdownCreateService.new(input_control, product, params[:qtde_oper]).call
-  #
-  #   redirect_to add_avaria_input_control_path(input_control)
-  # end
-
   def finish_conference
     @conference = Conference.where(id: params[:id])
     @result = Conferences::ConferenceFinishService.new(@conference).call
     flash_message @result
     redirect_to oper_input_controls_path
   end
-
-  # def finish_avaria
-  #   @input_control = InputControl.where(id: params[:id]).first
-  #   @input_control.update(date_finish_avaria: Date.today, time_finish_avaria: Time.now)
-  #   redirect_to oper_input_controls_path
-  # end
 
   def destroy_item
     conference_item = ConferenceItem.where(id: params[:id]).first
