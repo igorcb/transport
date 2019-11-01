64a50562569d88868178c3d08d0973b172f95d2a
Igor Batista <igor.batista@gmail.com>
2019-11-01 11:06:26 -0300

    add qtde nfe and qtde items list input_controls/sup

---
+++ app/services/comments/boarding_service.rb
@@ -0,0 +1,27 @@
+module CommentsService
+
+  def
+
+  # class CommentsService
+  #
+  #   def initialize(boarding)
+  #     @boarding = boarding
+  #   end
+  #
+  #   def call
+  #     #byebug
+  #     return {success: false, message: "Boarding is not found."} if !@conference.present?
+  #
+  #     begin
+  #       # byebug
+  #       ActiveRecord::Base.transaction do
+  #         #@conference.update(finish_time: Time.current, status: :finish, approved: :waiting)
+  #       end
+  #       return {success: true, message: "Conference on input_control created successfully."}
+  #     rescue => e
+  #       return {success: false, message: e.message}
+  #     end
+  #   end
+  # end
+
+end
