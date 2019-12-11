2319d0eeb8f1e9d2c916cb004013e7aedbbdd4c5
Thiago Lima <thiagolima86@gmail.com>
2019-12-11 13:33:35 -0300

    items in items pallet

--- app/controllers/palletizing_pallets_controller.rb
+++ app/controllers/palletizing_pallets_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class PalletizingPalletsController < ApplicationController
   before_action :set_palletizing
   def index
@@ -6,7 +7,19 @@ class PalletizingPalletsController < ApplicationController
 
   def new
     @pallet = Pallet.new
-    @items = @input_control.item_input_controls
+    view_mode = @palletizing.view_mode
+    if view_mode == "by_customer"
+      nfes = @input_control.nfe_xmls
+      data = []
+      nfes.each_with_index do |nfe, index|
+        data[index] = {group_name: nfe.numero.to_i, items: []}
+        nfe.item_input_controls.each do |item|
+          product = item.product
+          data[index][:items].push({item_id: item.id, product: "#{product.cod_prod} #{product.descricao}", qtde: item.qtde.to_i})
+        end
+      end
+    end
+    @items = data.to_json.html_safe
   end
 
   def create
