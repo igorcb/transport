e0202c1d504f566325c1447501a9e6d5ffabe706
Thiago Lima <thiagolima86@gmail.com>
2019-12-16 09:22:35 -0300

    service palletizing get by nfe

--- app/controllers/palletizing_pallets_controller.rb
+++ app/controllers/palletizing_pallets_controller.rb
@@ -7,24 +7,8 @@ class PalletizingPalletsController < ApplicationController
 
   def new
     @pallet = Pallet.new
-    view_mode = @palletizing.view_mode
-    if view_mode == "by_customer"
-      nfes = @input_control.nfe_xmls
-      data = []
-      nfes.each_with_index do |nfe, index|
-        data[index] = {group_name: nfe.numero.to_i, items: []}
-        nfe.item_input_controls.each do |item|
-          product = item.product
-          breakdown = @input_control.breakdown_nfe_xmls.where(nfe_xml_id: nfe, product_id: product.id).first
-          avarias = breakdown.avarias.to_i if breakdown.present?
-          faltas = breakdown.faltas.to_i if breakdown.present?
-          qtde = item.qtde.to_i - avarias.to_i - faltas.to_i
-          suggested_pallet = NfeXmls::CalcItemNfeQtdePalletService.new(product, qtde).call
-          data[index][:items].push({item_id: item.id, cod_prod: product.cod_prod, product_description: product.descricao, qtde: qtde, suggested_pallet: suggested_pallet[:qtde_pallet]})
-        end
-      end
-    end
-    @items = data.to_json.html_safe
+    data = PalletizingPallets::GetItemsService.new(@input_control).call
+    @items = data[:data].to_json.html_safe
   end
 
   def create
---
+++ app/services/palletizing_pallets/get_items_by_nfe_service.rb
@@ -0,0 +1,49 @@
+module PalletizingPallets
+  class GetItemsByNfeService
+
+    def initialize(input_control)
+      @input_control = input_control
+    end
+
+    def call
+
+      return {success: false, message: "Input control can't be blank."} if @input_control.blank?
+      @palletizing = @input_control.palletizing
+      return {success: false, message: "Paletizing can't be blank."} if @palletizing.blank?
+
+	    begin
+        ActiveRecord::Base.transaction do
+          data = []
+          index = 0
+          @input_control.nfe_xmls.each do |nfe|
+            data[index] = {group_name: nfe.numero.to_i, items: []}
+            nfe.item_input_controls.each do |item|
+              product = item.product
+              qtde_palletizing = @palletizing.palletizing_pallets.joins(:palletizing_pallet_products).where(:palletizing_pallet_products => {product_id: product.id, nfe_xml_id: nfe.id}).sum(:qtde)
+              puts ">>>>>>>>>>>>>>>>>>>>>>>>>#{qtde_palletizing}"
+
+              breakdown = @input_control.breakdown_nfe_xmls.where(nfe_xml_id: nfe, product_id: product.id).first
+              avarias = breakdown.avarias.to_i if breakdown.present?
+              faltas = breakdown.faltas.to_i if breakdown.present?
+              qtde = item.qtde.to_i - avarias.to_i - faltas.to_i - qtde_palletizing.to_i
+              suggested_pallet = NfeXmls::CalcItemNfeQtdePalletService.new(product, qtde).call
+              data[index][:items].push({item_id: item.id, cod_prod: product.cod_prod, product_description: product.descricao, qtde: qtde, suggested_pallet: suggested_pallet[:qtde_pallet]})
+            end
+
+            index += 1
+          end
+
+					return {success: true, message: "Calculation successful.", data: data}
+
+        end
+      rescue => e
+        puts e.message
+        return {success: false, message: e.message}
+      end
+
+    end
+
+    private
+
+  end
+end
