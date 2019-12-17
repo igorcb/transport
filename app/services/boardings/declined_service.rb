033f5c2144bf9b6f2ff6891e65c2a2168d532731
Igor Batista <igor.batista@gmail.com>
2019-12-17 10:13:31 -0300

    service declcined each items filter os id

--- app/services/boardings/declined_service.rb
+++ app/services/boardings/declined_service.rb
@@ -11,14 +11,14 @@ module Boardings
       return {success: false, message: "Boarding is not exist."} if @boarding.blank? && @user.blank?
       return {success: false, message: "Boarding must be in status EMBARCADO."} if @boarding.status != Boarding::TipoStatus::EMBARCADO
       return {success: false, message: "Ordem Services to boarding must be in status EMBARCADO."} if @boarding.boarding_items.includes(:ordem_service).where.not(ordem_services: {status: OrdemService::TipoStatus::EMBARCADO}).present?
-
+      #[]
       begin
         ActiveRecord::Base.transaction do
           Boarding.where(id: @boarding.id).update_all(status: Boarding::TipoStatus::REENTREGA)
           os_count_declined = 0
           os_ids = @boarding.boarding_items.pluck(:ordem_service_id)
           @boarding.boarding_items.each do |item|
-            ordem_service = OrdemService.where(id: item).first
+            ordem_service = OrdemService.where(id: item.ordem_service_id).first
             os_count_declined = ordem_service.declined
             ordem_service.status = OrdemService::TipoStatus::ABERTO
             ordem_service.declined += os_count_declined
