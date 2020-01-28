5a245acc39a1e07c810e4b83af52911731c280c0
Igor Batista <igor.batista@gmail.com>
2020-01-28 13:43:03 -0300

    edit complete devolutions

--- app/models/devolution.rb
+++ app/models/devolution.rb
@@ -1,2 +1,16 @@
 class Devolution < ApplicationRecord
+  validates :carrier_id, :driver_id, :billing_client_id, presence: true
+	validates :place, :place_horse, :place_cart_1, :date_entry, :time_entry, presence: true
+
+
+  belongs_to :carrier, required: false
+  belongs_to :driver, required: false
+  belongs_to :billing_client, class_name: "Client", foreign_key: "billing_client_id", required: false
+  belongs_to :user, required: false
+  belongs_to :breakdown_received, class_name: "User", foreign_key: "breakdown_user_id", required: false
+  belongs_to :user_received, class_name: "User", foreign_key: "received_user_id", required: false
+  belongs_to :started_user, class_name: "User", foreign_key: "started_user_id", required: false
+  belongs_to :billing_client, class_name: "Client", foreign_key: "billing_client_id", required: false
+
+
 end
