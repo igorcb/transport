10276709152ecfa28c4044db64056f2749d2dc7d
Thiago Lima <thiagolima86@gmail.com>
2020-01-30 13:12:29 -0300

    show add fields input control id, date receipt, created at, user email

--- app/models/palletizing.rb
+++ app/models/palletizing.rb
@@ -2,6 +2,7 @@ class Palletizing < ApplicationRecord
   validates :input_control, uniqueness: true
 
   belongs_to :input_control
+  belongs_to :user, foreign_key: 'user_created_id'
   has_many :palletizing_pallets, dependent: :destroy
 
   enum view_mode: [:by_customer, :by_nfe, :single]
