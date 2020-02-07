95754cffb1e073f0276b57ec0936200609265c17
Igor Batista <igor.batista@gmail.com>
2020-02-07 13:47:17 -0300

    service finish palletizing and update pallets in order_service

--- app/services/palletizing_pallets/pallets_by_nfe_service.rb
+++ app/services/palletizing_pallets/pallets_by_nfe_service.rb
@@ -12,14 +12,14 @@ module PalletizingPallets
 	    begin
         ActiveRecord::Base.transaction do
 
-          return PalletizingPallet.joins('LEFT JOIN "palletizing_pallet_products" ON  "palletizing_pallets"."id" = "palletizing_pallet_products"."palletizing_pallet_id"')
+          qtde_pallet = PalletizingPallet.joins('LEFT JOIN "palletizing_pallet_products" ON  "palletizing_pallets"."id" = "palletizing_pallet_products"."palletizing_pallet_id"')
                                   .joins('INNER JOIN "nfe_xmls" ON  "palletizing_pallet_products"."nfe_xml_id" = "nfe_xmls"."id"')
                                   .where(nfe_xmls: { numero: [@nfes]})
                                   .group("palletizing_pallets")
                                   .count("palletizing_pallets")
                                   .count
 					# return {success: true, message: "created successful."}
-
+          return qtde_pallet
         end
       rescue => e
         puts e.message
